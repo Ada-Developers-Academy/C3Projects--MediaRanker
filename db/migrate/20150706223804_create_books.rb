ec0f1369719dbf8fa655baf40b9c2c4a23e13c44
Loren Wang <wang131.131@gmail.com>
2015-07-06 16:20:38 -0700

    seeded my data into the db

--- db/migrate/20150706223804_create_books.rb
+++ db/migrate/20150706223804_create_books.rb
@@ -1,7 +1,11 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-
+      t.string :name
+      t.string :author
+      t.string :description
+      t.integer :rank
+      
       t.timestamps null: false
     end
   end
--- db/migrate/20150706223847_create_albums.rb
+++ db/migrate/20150706223847_create_albums.rb
@@ -1,6 +1,10 @@
 class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
+      t.string :name
+      t.string :artist
+      t.string :description
+      t.integer :rank
 
       t.timestamps null: false
     end
