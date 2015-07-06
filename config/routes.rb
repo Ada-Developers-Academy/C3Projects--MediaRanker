7269f6614672f180e92f4182507e88fdc434b37a
Kyna <khn710@gmail.com>
2015-07-06 16:19:24 -0700

    added routes, controllers, and test seed data

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'home#index'
 
+  resources :albums
+  resources :books
+  resources :movies
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+books = [
+  { title: "Sloth and Wrath", author: "Jane Austin", description: "Pride and Prejudice is a novel of manners by Jane Austen, first published in 1813. The story follows the main character Elizabeth Bennet as she deals with issues of sloth, wrath, morality, education, and marriage in the society of the landed gentry of early 19th-century England. Elizabeth is the second of five daughters of a country gentleman living near the fictional town of Meryton in Hertfordshire, near London.", ranking: 1}
+]
+
+books.each do |book|
+  Book.create book
+end
+
+movies = [
+  { title: "Bride and Prejudice", author: "Bollywood", description: "Pride and Prejudice as a musical", ranking: 1}
+]
+
+movies.each do |movie|
+  Movie.create movie
+end
+
+albums = [
+  { title: "20/20", author: "JT", description: "musaq", ranking: 1}
+]
+
+albums.each do |album|
+  Album.create album
+end
