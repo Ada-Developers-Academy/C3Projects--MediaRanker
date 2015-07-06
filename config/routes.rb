538638d4553344e71029bd32ca7fcfb7d02a586b
Shanna Chau <shannachau@gmail.com>
2015-07-06 16:35:16 -0700

    Created welcome controller, added header to layout, created index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+  resources :albums
+  resources :books
+  resources :movies
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
