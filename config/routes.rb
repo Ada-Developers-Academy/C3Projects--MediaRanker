6f3abff5986e6b733e50c9ea54dfc5c9103b7344
amirahaile <amira.dhaile@gmail.com>
2015-07-06 16:47:13 -0700

    Linked media items to their respective pages.

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  get '/movies' => 'movies#index'
-  get '/books'  => 'books#index'
-  get '/albums' => 'albums#index'
+  resources :movies
+  resources :books
+  resources :albums
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
