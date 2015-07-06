5babafbbbdc9c842a924c8cd37d8204788c6a39b
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-06 16:10:30 -0700

    Added resource routes & routes/actions to models.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :movies
+  resources :books
+  resources :albums
 
   # Example resource route with options:
   #   resources :products do
