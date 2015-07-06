216c363574c391bb3f97cc967fcb84c5cef9e82a
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-06 16:34:55 -0700

    created seed data, populated db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+media = [
+  { title: "The Stars My Destination", creator: "Alfred Bester", 
+    description: "a super cool book", format: "book" },
+  { title: "Anna Karenina", creator: "Leo Tolstoy", 
+    description: "a super cool book", format: "book" },
+  { title: "Dealing with Dragons", creator: "Patricia C. Wrede", 
+    description: "a super cool book", format: "book" },
+  { title: "Graceling", creator: "Kristin Cashore", 
+    description: "a super cool book", format: "book" },
+  { title: "The Way of Kings", creator: "Brandon Sanderson", 
+    description: "a super cool book", format: "book" },
+  { title: "Atonement", creator: "Ian McEwan", 
+    description: "a super cool book", format: "book" },
+  { title: "The Silly Book", creator: "Stoo Hample", 
+    description: "a super cool book", format: "book" },
+  { title: "Wind from a Foreign Sky", creator: "Katya Reimann", 
+    description: "a super cool book", format: "book" },
+
+  { title: "Ladyhawke", creator: "Richard Donner", 
+    description: "a super cool movie", format: "movie" },
+  { title: "Star Wars", creator: "George Lucas", 
+    description: "a super cool movie", format: "movie" },
+  { title: "Romancing the Stone", creator: "Robert Zemeckis", 
+    description: "a super cool movie", format: "movie" },
+  { title: "Guardians of the Galaxy", creator: "James Gunn", 
+    description: "a super cool movie", format: "movie" },
+  { title: "Harold and Maude", creator: "Hal Ashby", 
+    description: "a super cool movie", format: "movie" },
+  { title: "Jesus Christ Superstar", creator: "Norman Jewison", 
+    description: "a super cool movie", format: "movie" },
+  { title: "Wet Hot American Summer", creator: "David Wain", 
+    description: "a super cool movie", format: "movie" },
+
+  { title: "Stargate", creator: "Roland Emmerich", 
+    description: "a super cool movie", format: "movie" },
+  { title: "Reflektor", creator: "Arcade Fire", 
+    description: "a super cool album", format: "album" },
+  { title: "Sorry I'm Late", creator: "Cher Lloyd", 
+    description: "a super cool album", format: "album" },
+  { title: "Barton Hollow", creator: "Civil Wars", 
+    description: "a super cool album", format: "album" },
+  { title: "Rumours", creator: "Fleetwood Mac", 
+    description: "a super cool album", format: "album" },
+  { title: "Same Trailer Different Park", creator: "Kacey Musgraves", 
+    description: "a super cool album", format: "album" },
+  { title: "Animal", creator: "Ke$ha", 
+    description: "a super cool album", format: "album" },
+  { title: "Fantasies", creator: "Metric", 
+    description: "a super cool album", format: "album" },
+  { title: "Black Holes and Revelations", creator: "Muse", 
+    description: "a super cool album", format: "album" }
+]
+
+media.each do |medium|
+  Medium.create(medium)
+end
