9ef650a66a38ea6db16ac5b2350da64d993244f0
Jeri <drvonnjerryxlii@gmail.com>
2015-07-06 23:28:33 -0700

    mostly finished index pages for individual models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_active_record_name
 
-  def show; end
+  def show
+    @current = @active_record_name.find(params[:id])
+  end
+
   def new; end
   def edit; end
-  def index; end
+
+  def index
+    # active_record_name
+    @url_helper = controller_name
+    @all_records = @active_record_name.all
+  end
+
   def delete; end
+
+  private
+
+  def set_active_record_name
+    @active_record_name = eval(controller_name.classify.singularize)
+  end
 end
