2a4f2641419e0c2f8cf44af36e87b327c2c06b6d
amirahaile <amira.dhaile@gmail.com>
2015-07-06 18:04:14 -0700

    WIP: Button and links for media items' page.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,15 @@
 class MoviesController < ApplicationController
+  def index
+  end
+
   def show
     @movie = Movie.find(params[:id])
+  end
+
+  def destroy
+    movie = Movie.find(params[:id])
+    movie.destroy
+
+    redirect_to :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :movies
   resources :books
   resources :albums
+  resources :media, only: [:index, :show]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
