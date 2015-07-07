9858cba64be9f18068b5d7fd600cc4b75ee92be9
Shanna Chau <shannachau@gmail.com>
2015-07-06 17:33:29 -0700

    Created new movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,19 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(create_params[:task])
+    @movie.save
+  end
+
+  private
+
+  def create_params
+    params.permit(movie: [:name, :director, :description :ranking])
+  end
 end
