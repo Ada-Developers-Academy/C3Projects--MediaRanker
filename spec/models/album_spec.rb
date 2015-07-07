2faab5e093512ee0705251e34e8ef9a966d8ba55
Shanna Chau <shannachau@gmail.com>
2015-07-07 15:24:22 -0700

    WIP

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
+  describe "model validations" do
+    it "requires a name" do
+      album = Album.new
 
+      expect(album).to_not be_valid
+      expect(album.errors.keys).to include(:name)
+    end
+  end
+
+  describe "top scope" do
+
+      @album1 = Album.create(name: 'whatever', artist: 'some chick', description: 'whatever desc', ranking: 3)
+      @album2 = Album.create(name: 'whatever', artist: 'some dude', description: 'description', ranking: 1)
+      @album3 = Album.create(name: 'whatevz', artist: 'some chick', description: 'descrip', ranking: 2)
+      @album4 = Album.create(name: 'whatevah', artist: 'some dude', description: 'descrip', ranking: 4)
+
+      ordered_albums = [@album4, @album1, @album3]
+
+
+
+    it "orders albums according to ranking, highest to lowest" do
+      expect(Album.top(3)).to eq(ordered_albums)
+    end
+
+    it "only displays given number of albums" do
+      expect(Album.top(3).count).to eq 3
+    end
+  end
 end
