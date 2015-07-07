73cb636e19ba049016bbeaf5cffc277ae03b9ce0
Loraine <loraine@lorainekv.com>
2015-07-06 18:17:24 -0700

    Created a dynamic show for the media model.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,4 +1,12 @@
 class MediaController < ApplicationController
   def index
+    @movies = Medium.movies
+    @books = Medium.books
+    @albums = Medium.albums
+  end
+
+  def show
+    @medium = Medium.find(params[:id])
+    @type = @medium.media_type
   end
 end
