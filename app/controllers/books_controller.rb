4267959c981d0c5c9ff9ae391a9bcb4dcc24179b
Carly Jugler <carjug@gmail.com>
2015-07-07 16:29:36 -0700

    Creates index partial that doesn't work wahhhhhhh

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,24 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+
+    @books = @books.order('rank DESC')
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(create_params[:book])
+
+    redirect_to books_path
+  end
+
+  private
+
+  def create_params
+    params.permit(book: [:name, :author, :description, :rank])
   end
 
 end
