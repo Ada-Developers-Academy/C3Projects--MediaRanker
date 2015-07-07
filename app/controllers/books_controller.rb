dc9b505081a9c981750fad26f5a03999e1d4c23d
Lila Garcia <lilagrc@gmail.com>
2015-07-06 17:35:30 -0700

    Added show pages for each

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,12 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,4 +4,14 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    movie = Movie.find(params[:id])
+    movie.ranking += 1
+    movie.save
+  end
+
 end
