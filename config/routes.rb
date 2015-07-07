35bc32c098eeecb0afa4b2cd5ee6caa85edab0e3
Loraine <loraine@lorainekv.com>
2015-07-06 23:40:44 -0700

    Pulled the media tables out into dynamic partials.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'albums/index'
+
+  get 'books/index'
+
   get 'movies/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
