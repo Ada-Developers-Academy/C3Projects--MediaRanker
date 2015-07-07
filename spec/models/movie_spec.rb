34f3cf1c95da1cc85f591884b1cfce7c5144aed5
Shanna Chau <shannachau@gmail.com>
2015-07-07 16:21:10 -0700

    Completed specs for movie model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  
+  describe "model validations" do
+    it "requires a name" do
+      movie = Movie.new
+
+      expect(movie).to_not be_valid
+      expect(movie.errors.keys).to include(:name)
+    end
+  end
+
+  describe "top scope" do
+    before :each do
+      @movie1 = Movie.create(name: 'whatever', director: 'some chick', description: 'whatever desc', ranking: 3)
+      @movie2 = Movie.create(name: 'whatever', director: 'some dude', description: 'description', ranking: 1)
+      @movie3 = Movie.create(name: 'whatevz',  director: 'some chick', description: 'descrip', ranking: 2)
+      @movie4 = Movie.create(name: 'whatevah', director: 'some dude', description: 'descrip', ranking: 4)
+    end
+
+    it "orders movies according to ranking, highest to lowest" do
+      top_movies = [@movie4, @movie1, @movie3]
+      expect(Movie.top(3)).to eq(top_movies)
+    end
+
+    it "only displays given number of movies" do
+      expect(Movie.top(3).count).to eq 3
+    end
+  end
 end
