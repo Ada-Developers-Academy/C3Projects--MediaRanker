5e8a4374c1228f3a7b1c0f8d63fa2aade2903a6e
Shanna Chau <shannachau@gmail.com>
2015-07-07 14:27:11 -0700

    Changing index to display top 10 of each media

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @top_movies = Movie.all.top(5)
-    @top_books = Book.all.top(5)
-    @top_albums = Album.all.top(5)
+    @top_movies = Movie.all.top(10)
+    @top_books = Book.all.top(10)
+    @top_albums = Album.all.top(10)
   end
 end
