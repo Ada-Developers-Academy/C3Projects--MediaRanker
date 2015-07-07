a8c594c0f6defd85907bcbaf0dff3cce509fdd51
Elia <elgrenier@gmail.com>
2015-07-06 18:45:11 -0700

    edited the home page to show the top 10 and created the movies show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,5 +7,25 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+  
+  def add_a_vote
+    @movie = Movie.find(params[:id])
+    @movie.rank += 1
+  end
+
+  def upvote
+    @movie = Movie.find(params[:id])
+    @movie.add_a_vote
+    @movie.update(create_params[:rank])
+    render :show
+  end
+
+
+  private
+
+  def create_params
+    params.permit(movie: [:name, :director, :description, :rank])
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :movies
   resources :albums
   # Example of regular route:
-  # get 'products/:id' => 'catalog#view'
+  patch 'movies/upvote/:id' => 'movies#upvote', as: "movie_upvote"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
