21e9dcccb6d8067bf0d1e6a77bee88384b7a65f0
Elia <elgrenier@gmail.com>
2015-07-07 16:12:20 -0700

    6 passing tests, added tests for scope top

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  describe "model validations" do
+  describe "model validation" do
     it "requires a name" do
       album = Album.new
 
@@ -9,4 +9,20 @@ RSpec.describe Album, type: :model do
       expect(album.errors.keys).to include(:name)
     end
   end
+
+  describe "top scope" do
+    it "returns albums in descending rank order" do
+    album1 = Album.create(name: "Dark Side of the Moon", creator: "Pink Floyd", description: "Classic rock album", rank: 50)
+    album2 = Album.create(name: "Truly Madly Deeply", creator: "Savage Garden", description: "Early nineties music that will get stuck in your head", rank: 20)
+    album3 = Album.create(name: "Tracy Chapman", creator: "Tracy Chapman", description: "Debut album, full of classic songs", rank: 98)
+    album4 = Album.create(name: "To the Teeth", creator: "Ani DiFranco", description: "First real live show I went to", rank: 60)
+
+    correct_order = [album3, album4, album1, album2]
+    expect(Album.top).to eq correct_order
+    end
+  end
+
+
+
+
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  describe "model validations" do
+  describe "model validation" do
     it "requires a name" do
       book = Book.new
 
@@ -9,4 +9,17 @@ RSpec.describe Book, type: :model do
       expect(book.errors.keys).to include(:name)
     end
   end
+
+  describe "top scope" do
+    it "returns books in descending rank order" do
+    book1 = Book.create(name: "Pride and Prejudice", creator: "Jane Austen", description: "Classic literature. Kelli has many many copies", rank: 50)
+    book2 = Book.create(name: "1984", creator: "George Orwell", description: "Dystopian future of Big Brother", rank: 60)
+    book3 = Book.create(name: "The Kite Runner", creator: "Khaled Hosseini", description: "Childhood in Kabul, friendship and kites", rank: 68)
+    book4 = Book.create(name: "To Kill a Mockingbird", creator: "Harper Lee", description: "Childhood in a sleepy southern town", rank: 40)
+
+    correct_order = [book3, book2, book1, book4]
+    expect(Book.top).to eq correct_order
+    end
+  end
+
 end
