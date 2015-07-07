9d1f5440d33f45179ed8d1c60b16d102ebe7161c
Elia <elgrenier@gmail.com>
2015-07-07 15:37:55 -0700

    3 passing tests for validating name required- only required field

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    it "requires a name" do
+      book = Book.new
+
+      expect(book).to_not be_valid
+      expect(book.errors.keys).to include(:name)
+    end
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    it "requires a name" do
+      movie = Movie.new
+
+      expect(movie).to_not be_valid
+      expect(movie.errors.keys).to include(:name)
+    end
+  end
 end
