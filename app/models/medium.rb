81800ba4f1477fd7f87dc80eb1a0a803fdf8ea9c
Loraine <loraine@lorainekv.com>
2015-07-07 14:28:38 -0700

    Created test for creating new records.

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -3,7 +3,7 @@ class Medium < ActiveRecord::Base
   #------ VALIDATIONS ------
 
   # validates :ranking, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  # validates :name, presence: true
+  validates :name, presence: true
   # validates :contributor, presence: true
   # validates :description, presence: true
   # validates :user, presence: true
--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe MediaController, type: :controller do
 
+  describe "GET #index" do
+      it "responds successfully with an HTTP 200 status code" do
+        get :index
+
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+    end
+
+  describe "POST #create" do
+    #positive test - when the media params are valid
+    context "Vaid media params" do
+      let (:medium_params) do
+        {
+          medium: {
+            ranking: "2",
+            name: "Book name",
+            contributor: "Book author",
+            description: "Book desc",
+            user: "Chococat",
+            media_type: "book"
+          }
+        }
+      end
+
+      it "creates an media record" do
+        post :create, medium_params
+        expect(Medium.count).to eq 1
+      end
+    end
+  end
 end
