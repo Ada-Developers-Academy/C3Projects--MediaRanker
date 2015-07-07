11c5787c35de62ce6d8b1d3dc5ba86471f641115
amirahaile <amira.dhaile@gmail.com>
2015-07-07 15:51:42 -0700

    Wrote specs for Albums#show.

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe AlbumsController, type: :controller do
+  describe "GET #show" do
+    it "locates the correct Album" do
+      album1 = Album.new(id: 1, title: "a title")
+      album1.save
+      album2 = Album.new(id: 2, title: "b title")
+      album2.save
+      params = { id: 2 }
 
+      expect(Album.find(params[:id]).title).to eq("b title")
+    end
+
+    it "renders the :show view" do
+      album1 = Album.new(id: 1, title: "a title")
+      album1.save
+      get :show, id: 1
+      expect(response).to render_template("show")
+    end
+  end
 end
