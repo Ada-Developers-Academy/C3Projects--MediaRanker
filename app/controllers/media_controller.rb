109412e40d437f01eb12c7b9a9c940ef8f455b87
Loraine <loraine@lorainekv.com>
2015-07-06 22:00:25 -0700

    Created upvote feature.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -9,4 +9,11 @@ class MediaController < ApplicationController
     @medium = Medium.find(params[:id])
     @type = @medium.media_type
   end
+
+  def upvote
+    @medium = Medium.find(params[:id])
+    @medium.ranking += 1
+    @medium.save
+    redirect_to(medium_path)
+  end
 end
