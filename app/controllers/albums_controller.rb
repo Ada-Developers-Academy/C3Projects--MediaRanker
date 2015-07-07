fb7f358570d19bb6efdde09eed2b6347704dcead
Kyna <khn710@gmail.com>
2015-07-07 15:07:36 -0700

    WIP: completing methods for album controller + views

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,7 +4,10 @@ class AlbumsController < ApplicationController
   end
 
   def new
-    @album = Album.new
+    @media = Album.new
+    @url = albums_path
+    @method = :post
+    @by = :artist
   end
 
   def create
@@ -13,11 +16,33 @@ class AlbumsController < ApplicationController
     redirect_to albums_path
   end
 
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def edit
+    @media = Album.find(params[:id])
+    @url = album_path
+    @method = :patch
+    @by = :artist
+  end
+
+  def update
+    @media = Album.find(params[:id])
+    @media.update(album_params)
+    @media.save
+    redirect_to albums_path
+  end
+
+  def destroy
+    Album.find(params[:id]).destroy
+    redirect_to albums_path
+  end
 
 private
 
   def album_params
-    params.require(:album).permit(:title, :author, :description)
+    params.require(:album).permit(:title, :author, :description, :ranking)
   end
 
 end
