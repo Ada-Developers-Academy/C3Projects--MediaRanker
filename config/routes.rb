c78884cdaf05614cb0767eae541c31ff67f9d9ec
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-07 13:38:15 -0700

    made resources routes all 1 line.

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  resources :movies
-  resources :albums
-  resources :books
+  resources :movies, :albums, :books
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
