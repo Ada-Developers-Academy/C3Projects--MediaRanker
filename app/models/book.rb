eee5edf066142a2acf9f376c6236fd08573da814
Sally Moore <sallyamoore@gmail.com>
2015-07-07 15:47:06 -0700

    Added validations to all models. Also edited the title in layout.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ActiveRecord::Base
+  # Validations -------------
+  validates :id,    presence: true,
+                    numericality: { only_integer: true },
+                    uniqueness: true
+  validates :title, presence: true,
+                    length: { minimum: 1 }
+  validates :rank,  presence: true,
+                    numericality: { only_integer: true }
+
+  # Scopes -------------
   scope :top, -> { order(rank: :desc).limit(5) }
 end
