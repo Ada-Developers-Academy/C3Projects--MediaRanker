e77b64a414dc0921e668b945c9ad16ef7f209c25
Kari <kari.lonczak@gmail.com>
2015-07-06 17:49:19 -0700

    Created partials for the show and index views for books/movies/albums. WIP on customizing the indiv media path links in the _index partial.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :movies
+  resources :books
+  resources :albums
+
   # Example resource route with options:
   #   resources :products do
   #     member do
