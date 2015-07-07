7a32577880e2791ddc998c6badd351cee940f5c9
Carly Jugler <carjug@gmail.com>
2015-07-06 17:56:02 -0700

    Adds create, edit, and delete functionality to albums views.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,4 +4,42 @@ class AlbumsController < ApplicationController
     @albums = Album.all
   end
 
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.create(create_params[:album])
+
+    redirect_to albums_path
+  end
+
+  def show
+    @album = Album.find(params[:id])
+
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    @album.update(create_params[:album])
+
+    redirect_to album_path(@album.id)
+  end
+
+  def destroy
+    @album = Album.destroy(params[:id])
+
+    redirect_to albums_path
+  end
+
+  private
+
+  def create_params
+    params.permit(album: [:name, :artist, :description, :rank])
+  end
 end
