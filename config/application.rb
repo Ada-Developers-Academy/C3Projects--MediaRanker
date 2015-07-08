8c25c758d9074894f8f789c3e37a51143bc8d0fb
Loren Wang <wang131.131@gmail.com>
2015-07-08 14:20:42 -0700

    override default of changing html structure when edit form doesn't save with require d fields

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module C3ProjectsMediaranker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+  html_tag
+}
   end
 end
