0053d29f35e8c465dcec26f757514dd482ffe3d7
Kyna <khn710@gmail.com>
2015-07-08 15:10:44 -0700

    WIP: adding basic bootstrap, troubleshooting custom error messages

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,5 +4,10 @@ class Album < ActiveRecord::Base
 validates :title, presence: true
 validates :author, presence: true
 validates :ranking, presence: true, numericality: {only_integer: true}
+
+  def validate_or_msg
+    errors[:title] = "Every album needs a title ya silly"
+    errors[:author] = "Well someone created it, right?"
+  end
 # Scopes --------------------------------------
 end
