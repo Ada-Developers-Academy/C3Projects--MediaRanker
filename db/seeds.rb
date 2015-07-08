2d15d2c5099a37257ce99d1502640b5f2ddd9d07
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-08 14:40:23 -0700

    Updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 movies = [
-  { title: "Puppies", directed_by: "Dog", votes: 10, description: "" },
-  { title: "Kitties", directed_by: "Cat", votes: 9, description: "" },
-  { title: "Fishies", directed_by: "Fish", votes: 0, description: "" },
-  { title: "Llamas", directed_by: "The Llama", votes: 6, description: "" },
-  { title: "Orangutans", directed_by: "Orangutan", votes: 7, description: "" },
-  { title: "Elephants", directed_by: "Elephant", votes: 0, description: "" }
+  { title: "Puppies Movie", directed_by: "Dog", votes: 10, description: "" },
+  { title: "Kitties Movie", directed_by: "Cat", votes: 9, description: "" },
+  { title: "Fishies Movie", directed_by: "Fish", votes: 0, description: "" },
+  { title: "Llamas Movie", directed_by: "The Llama", votes: 6, description: "" },
+  { title: "Orangutans Movie", directed_by: "Orangutan", votes: 7, description: "" },
+  { title: "Elephants Movie", directed_by: "Elephant", votes: 0, description: "" }
 ]
 
 movies.each do |movie|
@@ -20,12 +20,12 @@ movies.each do |movie|
 end
 
 books = [
-  { title: "Puppies", written_by: "Dog", votes: 10, description: "" },
-  { title: "Kitties", written_by: "Cat", votes: 9, description: "" },
-  { title: "Fishies", written_by: "Fish", votes: 0, description: "" },
-  { title: "Llamas", written_by: "The Llama", votes: 6, description: "" },
-  { title: "Orangutans", written_by: "Orangutan", votes: 7, description: "" },
-  { title: "Elephants", written_by: "Elephant", votes: 0, description: "" }
+  { title: "Puppies Book", written_by: "Dog", votes: 10, description: "" },
+  { title: "Kitties Book", written_by: "Cat", votes: 9, description: "" },
+  { title: "Fishies Book", written_by: "Fish", votes: 0, description: "" },
+  { title: "Llamas Book", written_by: "The Llama", votes: 6, description: "" },
+  { title: "Orangutans Book", written_by: "Orangutan", votes: 7, description: "" },
+  { title: "Elephants Book", written_by: "Elephant", votes: 0, description: "" }
 ]
 
 books.each do |book|
@@ -33,12 +33,12 @@ books.each do |book|
 end
 
 albums = [
-  { title: "Puppies", recorded_by: "Dog", votes: 10, description: "" },
-  { title: "Kitties", recorded_by: "Cat", votes: 9, description: "" },
-  { title: "Fishies", recorded_by: "Fish", votes: 0, description: "" },
-  { title: "Llamas", recorded_by: "The Llama", votes: 6, description: "" },
-  { title: "Orangutans", recorded_by: "Orangutan", votes: 7, description: "" },
-  { title: "Elephants", recorded_by: "Elephant", votes: 0, description: "" }
+  { title: "Puppies Album", recorded_by: "Dog", votes: 10, description: "" },
+  { title: "Kitties Album", recorded_by: "Cat", votes: 9, description: "" },
+  { title: "Fishies Album", recorded_by: "Fish", votes: 0, description: "" },
+  { title: "Llamas Album", recorded_by: "The Llama", votes: 6, description: "" },
+  { title: "Orangutans Album", recorded_by: "Orangutan", votes: 7, description: "" },
+  { title: "Elephants Album", recorded_by: "Elephant", votes: 0, description: "" }
 ]
 
 albums.each do |album|
