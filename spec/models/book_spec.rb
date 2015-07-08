cc5c54156f7c101b11610692f4144207ec445717
Carly Jugler <carjug@gmail.com>
2015-07-08 12:36:45 -0700

    Adds working/passing validation specs to book and movie spec.rb files.

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+
+    before :each do
+      @book = Book.new
+      @book.save
+    end
+
+    it "requires a name all the time" do
+      expect(@book.errors.keys).to include(:name)
+    end
+
+    it "requires a rank all the time" do
+      expect(@book.errors.keys).to include(:rank)
+    end
+  end
+
+  context "validating rank" do
+    it "requires rank to be an integer" do
+      @book = Book.new(name: "Lovely", rank: "hi" )
+
+      @book.save
+
+      expect(@book.errors.keys).to include(:rank)
+    end
+
+    it "requires rank to be less than 200" do
+      @book = Book.new(name: "Lovely", rank: 250)
+
+      @book.save
+
+      expect(@book.errors.keys).to include(:rank)
+    end
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+
+    before :each do
+      @movie = Movie.new
+      @movie.save
+    end
+
+    it "requires a name all the time" do
+      expect(@movie.errors.keys).to include(:name)
+    end
+
+    it "requires a rank all the time" do
+      expect(@movie.errors.keys).to include(:rank)
+    end
+  end
+
+  context "validating rank" do
+    it "requires rank to be an integer" do
+      @movie = Movie.new(name: "Lovely", rank: "hi" )
+
+      @movie.save
+
+      expect(@movie.errors.keys).to include(:rank)
+    end
+
+    it "requires rank to be less than 200" do
+      @movie = Movie.new(name: "Lovely", rank: 250)
+
+      @movie.save
+
+      expect(@movie.errors.keys).to include(:rank)
+    end
+  end
 end
