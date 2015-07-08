b84f5614785b203412f802df3125ac93c0072620
Ashley Watkins <awatkins31@gmail.com>
2015-07-07 17:42:15 -0700

    Adds validation to require title

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,8 @@
 class Album < ActiveRecord::Base
   scope :best, -> (total) { order('votes DESC').limit(total) }
 
+  validates :title, presence: true
+
   def self.upvote(album)
      album.votes += 1
      album.save
