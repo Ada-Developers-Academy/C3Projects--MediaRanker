b8d515c7bb4e0713346443bba4f8a89d1fe33490
Corinne Pingul <corinnepingul@gmail.com>
2015-07-08 16:22:25 -0700

    Added methods to BooksController so that it can edit and delete records.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,30 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if params[:book].nil? == true
+      @book.rank += 1
+      @book.save
+    else
+      @book.update(book_params)
+    end
+
+    render :show
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def book_params
+    params[:book].permit(:title, :author, :description, :rank)
+  end
 end
