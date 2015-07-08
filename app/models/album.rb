c34bebf7ea0193483cebefaeae7099e953b74781
Carly Jugler <carjug@gmail.com>
2015-07-07 20:52:05 -0700

    Adds validations to model/album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,13 @@
 class Album < ActiveRecord::Base
+ # Associations!
   belongs_to :user
 
+# Validations!
+  validates :name, presence: true, uniqueness: true
+  validates :rank, presence: true,
+            numericality: { only_integer: true, less_than: 200 }
+
+
+# Scopes!
   scope :best, -> { order('rank DESC').limit(10) }
 end
