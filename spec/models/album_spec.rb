8c3a8d5a80c0a8b0da394811b24d10eecfad0747
Carly Jugler <carjug@gmail.com>
2015-07-08 12:29:29 -0700

    Adds working/passing model validation specs for name and rank to album_spec

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,12 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Album, type: :model do
   describe "model validations" do
-    it "requires a name all the time" do
-      album = Album.new
 
-      # expect(album).to_not be_valid
-      expect(album.errors.keys).to include(:name)
+    before :each do
+      @album = Album.new
+      @album.save
     end
 
+    it "requires a name all the time" do
+      expect(@album.errors.keys).to include(:name)
+    end
+
+    it "requires a rank all the time" do
+      expect(@album.errors.keys).to include(:rank)
+    end
+  end
+
+  context "validating rank" do
+    it "requires rank to be an integer" do
+      @album = Album.new(name: "Lovely", rank: "hi" )
+
+      @album.save
+
+      expect(@album.errors.keys).to include(:rank)
+    end
+    it "requires rank to be less than 200" do
+      @album = Album.new(name: "Lovely", rank: 250)
+
+      @album.save
+
+      expect(@album.errors.keys).to include(:rank)
+    end
   end
 end
+
