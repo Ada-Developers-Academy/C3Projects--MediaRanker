2dcc396506a19459e473648499c2243d79795b87
Ashley Watkins <awatkins31@gmail.com>
2015-07-08 13:32:46 -0700

    Adds requisite  line to include the shared tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared_tests_for_models.rb'
 
 RSpec.describe Book, type: :model do
   describe "model methods" do
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared_tests_for_models.rb'
 
 RSpec.describe Movie, type: :model do
   describe "model methods" do
