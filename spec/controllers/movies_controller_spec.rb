bc2765bf76dacf10ccff084e5a68bc8a6544d8b5
amirahaile <amira.dhaile@gmail.com>
2015-07-08 15:24:50 -0700

    Added 2 specs for Movies#update.

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -31,6 +31,51 @@ RSpec.describe MoviesController, type: :controller do
     end
   end
 
+  describe "GET #edit" do
+    # copy pasta'd from GET #show
+    # is this even good?
+    it "locates the correct Movie" do
+      @movie1 = Movie.new(id: 1, title: "a title")
+      @movie1.save
+      movie2 = Movie.new(id: 2, title: "b title")
+      movie2.save
+      params = { id: 2 }
+
+      expect(Movie.find(params[:id]).title).to eq("b title")
+    end
+  end
+
+  describe "PUT #update" do
+    before :each do
+      @movie1 = Movie.new(id: 1, title: "a title")
+      @movie1.save
+      @movie2 = Movie.new(id: 2, title: "b title")
+      @movie2.save
+      Movie.new(id: 3, title: "c title").save
+    end
+
+    it "locates the correct Movie" do
+      params = { id: 1 }
+      expect(Movie.find(params[:id]).title).to eq("a title")
+    end
+
+    # collects the information from the user
+    # it "collects the user input" do
+    #   params = { movie: { title: "Reservoir Dogs", director: "Quentin Tarantino", description: ""} }
+    #   put :update, params[:movie]
+    # end
+
+    # updates/saves the movie – doesn't create a new object
+
+    # redirects to the movie's page
+    it "redirects to the movie's :show view" do
+      params = { movie: {"title"=>"Reservoir Dogs", "director"=>"Quentin Tarantino", "description"=>""}, id: "2" }
+      put :update, params
+      
+      expect(subject).to redirect_to(movie_path(@movie2))
+    end
+  end
+
   describe "DELETE #destroy" do
     before :each do
       @movie1 = Movie.new(id: 1, title: "a title")
