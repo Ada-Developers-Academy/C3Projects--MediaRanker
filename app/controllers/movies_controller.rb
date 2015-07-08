3e53d638f22adee9d2f8a83dea11d05eeb6bf827
amirahaile <amira.dhaile@gmail.com>
2015-07-08 14:50:13 -0700

    Added Movies#update.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,10 +11,25 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def update
+    movie = Movie.find(params[:id])
+    updated_movie = create_params[:movie]
+    # opportunity for .valid? and error handling
+    movie.update(updated_movie)
+
+    redirect_to movie_path(movie)
+  end
+
   def destroy
     movie = Movie.find(params[:id])
     movie.destroy
 
     redirect_to media_path
+  end
+
+  private
+
+  def create_params
+    params.permit(movie: [:title, :director, :description])
   end
 end
