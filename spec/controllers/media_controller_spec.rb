809ce6fb9a84126eff56b6c1863edf96143cfab2
Loraine <loraine@lorainekv.com>
2015-07-08 15:08:51 -0700

    Working on controller specs.

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe MediaController, type: :controller do
 
+  describe "GET #index" do
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "loads media into their respective @movies, @books, and @albums" do
+      ["book", "movie", "album"].each do |type|
+      medium1 = Medium.create!(name: "new1", media_type: type)
+      medium2 = Medium.create!(name: "new2", media_type: type)
+      end
+
+      expect(assigns(:book)).to match_array([medium1, medium2])
+    end
+
+
+  end
+
   describe "POST #create" do
     #positive test - when the media params are valid
     context "Valid media params" do
@@ -43,14 +62,24 @@ RSpec.describe MediaController, type: :controller do
     it { expect(@medium.description).to eq attr[:description] }
   end
 
-  # describe "DELETE #destroy" do
-  #
-  #   context "when deletion succeeds" do
-  #
-  #     it "deletes the medium record" do
-  #       @medium = Medium.create!(ranking: "4", name: "Book name", media_type: "book")
-  #       expect { delete :destroy, :id => @medium.id }.to change(Medium, :count).by(-1)
-  #     end
-  #   end
-  # end
+  describe "DELETE #destroy" do
+
+    context "when deletion succeeds" do
+
+      # let (:params) do
+      #   {
+      #     from: "book"
+      #   }
+      # end
+
+      it "deletes a medium record" do
+        @medium = Medium.create!(ranking: "4", name: "Book name", media_type: "book")
+        expect { delete :destroy, :id => @medium.id, :from => "book" }.to change(Medium, :count).by(-1)
+        # Since my redirect filepath relies on a "from" key in params, I included a specific key/value here
+      end
+    end
+  end
+
+
+
 end
