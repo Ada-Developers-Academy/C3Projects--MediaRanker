d6c7c35ffe9495fa31229514866ffb2c2d419114
Brenna <brennaleker@gmail.com>
2015-07-08 13:56:04 -0700

    WIP - getting rspec to play nice

---
+++ spec/models/video_game_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+#
+# RSpec.describe VideoGame, type: :model do
+#   it "orders by ranking descending" do
+#     street_fighter = VideoGame.create!(title: "Street Fighter", stars: 12)
+#     smash_bros = VideoGame.create!(title: "Super Mario Smash Brothers", stars: 24)
+#
+#     expect(VideoGame.ordered_by_stars).to eq([smash_bros, street_fighter])
+#   end
+# end
