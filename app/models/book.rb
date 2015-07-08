ae31b5ba0ba7798ba03fd212a96d0ee74dbc8593
Ashley Watkins <awatkins31@gmail.com>
2015-07-07 17:41:04 -0700

    Adds validation to require title

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ActiveRecord::Base
   scope :best, -> (total) { order('votes DESC').limit(total) }
 
+  validates :title, presence: true
+
   def self.upvote(book)
      book.votes += 1
      book.save
