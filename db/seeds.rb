7525b12145a2656acdce2cf7c9ed805e471045c9
Michelle McCarthy <michellemccarthy@hotmail.com>
2015-07-07 23:30:31 -0700

    Added 2 seed movies to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+movies = [
+  { title: "That Thing You Do!", director: "Tom Hanks", description: "Early boyband hijinx with a musical twist!", vote: 40},
+  { title: "Charlies Angels", director: "Bret Ratner", description: "Three crime fighting heroines with fabulous hair solve crimes.", vote: 22},
+]
+movies.each do |movie|
+  Movie.create movie
+end
