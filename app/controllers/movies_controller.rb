2de65e08bbea3ae637b7ebf67802f6c4d7f2548f
Loren Wang <wang131.131@gmail.com>
2015-07-07 19:13:34 -0700

    fixed edit form when name is blank and ranking = 0 for newly added movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,22 @@ class MoviesController < ApplicationController
    @movies = Movie.all
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(create_params[:movie])
+    @movie.rank = 0
+
+    if @movie.save
+      render :show
+    else
+      render :new
+    end
+# redirect_to movie_path(@movie.id)
+  end
+
   def show
     @movie = Movie.find(params[:id])
   end
@@ -20,7 +36,13 @@ class MoviesController < ApplicationController
     description_input = params[:movie][:description]
 
     @movie.update(name: name_input, director: director_input , description: description_input)
-    render :show
+
+    if @movie.save
+      redirect_to movies_path(@movie.id)
+    else
+      render :edit
+    end
+
 
     # redirect_to movie_path(@movie.id) #movie/:id
   end
@@ -43,5 +65,8 @@ class MoviesController < ApplicationController
     params.permit(movie: [:name, :director, :description])
   end
 
+  def create_params
+    params.permit(movie: [:name, :director, :description])
+  end
 
 end
