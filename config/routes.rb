9316d763d0443a2c0e0da317e6b61ce0a5dee446
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-07 22:40:54 -0700

    added routes for upvote

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  put '/movies/:id/upvote' => 'movies#upvote', as: 'upvote_movie'
+  put '/movies/:id/upvote' => 'movies#upvote', as: 'movie_upvote'
+  put '/books/:id/upvote' => 'books#upvote', as: 'book_upvote'
+  put '/albums/:id/upvote' => 'albums#upvote', as: 'album_upvote'
 
   resources :movies
   resources :books
