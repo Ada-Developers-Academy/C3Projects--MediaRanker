62a2cb3e9e2ea98b689677364bb9fdfdcce00abb
Corinne Pingul <corinnepingul@gmail.com>
2015-07-08 15:04:48 -0700

    Wrote partial for showing all the albums, movies, and books. Need to apply it to books and movies. I also need to add to the book controller all the edit, update, destroy methods.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,6 @@
 class AlbumsController < ApplicationController
   def index
-
+    @albums = Album.all.rank_order
   end
 
   def show
@@ -9,5 +9,31 @@ class AlbumsController < ApplicationController
 
   def edit
     @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    if params[:album].nil? == true
+      @album.rank += 1
+      @album.save
+    else
+      @album.update(album_params)
+    end
+
+    render :show
+  end
+
+  def destroy
+    album = Album.find(params[:id])
+    album.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def album_params
+    params[:movie].permit(:title, :artist, :description, :rank)
   end
 end
