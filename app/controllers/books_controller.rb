c9d0fe5183ce0c94832dbe2fd4e9dbe13bd7f138
Carly Jugler <carjug@gmail.com>
2015-07-07 17:10:31 -0700

    Adds new form partial for all media controllers.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,29 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
+  def show
+    @book = Book.find(params[:id])
+
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    @book.update(create_params[:book])
+
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.destroy(params[:id])
+
+    redirect_to books_path
+  end
+
   private
 
   def create_params
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,48 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+
+    @movies = @movies.order('rank DESC')
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(create_params[:movie])
+
+    redirect_to movies_path
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    @movie.update(create_params[:movie])
+
+    redirect_to movie_path(@movie.id)
+  end
+
+  def destroy
+    @movie = Movie.destroy(params[:id])
+
+    redirect_to movies_path
+  end
+
+  private
+
+  def create_params
+    params.permit(movie: [:name, :author, :description, :rank])
   end
 
 end
+
