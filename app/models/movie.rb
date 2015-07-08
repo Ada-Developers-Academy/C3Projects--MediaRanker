929fef564eb5852e4584f60307680627a61f34c6
Victoria <vikshab2010@hotmail.com>
2015-07-07 17:20:27 -0700

    WIP

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
+# Validations ________________________________________________________________
+  # validates :name, presence: true
+
+# Scope_______________________________________________________________________
   scope :best, -> { order('ranking DESC').limit(10) }
 end
