c3a3c08a079ff54e5478e62ea904f5ea05c057ec
Ashley Watkins <awatkins31@gmail.com>
2015-07-07 17:36:47 -0700

    Adds validation to require the title

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
   scope :best, -> (total) { order('votes DESC').limit(total) }
 
+  validates :title, presence: true
+
   def self.upvote(movie)
      movie.votes += 1
      movie.save
