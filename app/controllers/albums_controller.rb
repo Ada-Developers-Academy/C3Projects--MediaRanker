5a3cf601c2479d0d586b4c81f05ef722f722a70b
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-08 19:29:34 -0700

    Adds ability to add a new album and creates a new view using form partial

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,6 +5,19 @@ class AlbumsController < ApplicationController
     @albums = Album.all
   end
 
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(create_params)
+    if @album.save
+      redirect_to album_path(@album)
+    else
+      render :new
+    end
+  end
+
   def update
     if params[:upvote] == "true"
       Album.upvote(@album)
@@ -20,5 +33,9 @@ class AlbumsController < ApplicationController
   private
   def set_album
     @album = Album.find(params[:id])
+  end
+
+  def create_params
+    params.require(:album).permit(:title, :recorded_by, :description)
   end
 end
