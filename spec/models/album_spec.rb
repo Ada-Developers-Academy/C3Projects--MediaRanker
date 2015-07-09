1021e9a6885c63d5f28f5bd392c0a49eefa5b2ea
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-08 23:33:10 -0700

    added tests for Album scope :ranked

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -20,4 +20,24 @@ RSpec.describe Album, type: :model do
       end
     end
   end
+
+  describe "ranked scope" do
+    # positive test
+    it "orders the albums descending by rank" do
+      album1 = Album.create(title: 'a', rank: 1)
+      album2 = Album.create(title: 'b', rank: 2)
+      album3 = Album.create(title: 'c', rank: 3)
+
+      expect(Album.ranked.first).to eq album3
+    end
+
+    # negative test
+    it "does not order the albums by id" do
+      album1 = Album.create(title: 'a', rank: 1)
+      album2 = Album.create(title: 'b', rank: 2)
+      album3 = Album.create(title: 'c', rank: 3)
+
+      expect(Album.ranked.first).to_not eq Album.find(1)
+    end
+  end
 end
