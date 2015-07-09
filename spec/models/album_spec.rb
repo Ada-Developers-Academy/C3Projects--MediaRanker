f34165cb58dbd070fa1e27e0954a8d9224263ae6
Elsa <elsa.moluf@gmail.com>
2015-07-08 18:42:32 -0700

    Have model rspec tests for all media.

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -7,8 +7,33 @@ RSpec.describe Album, type: :model do
       album = Album.new
 
       expect(album).to_not be_valid
-      expect(album.errors.keys).to include(:title)
+      expect(album.errors.keys).to include(:name)
     end
+  end
+
+  describe "has top albums" do
+    before :each do
+      @album0 = Album.create(name: "zero", rank: 0)
+      album1 = Album.create(name: "one", rank: 1)
+      album2 = Album.create(name: "two", rank: 2)
+      album3 = Album.create(name: "three", rank: 3)
+
+      @top_albums = [album3, album2, album1]
+    end
+
+
+    it "lists the top three albums" do
+      expect(Album.top.count).to eq(3)
+    end
+
+    it "ranks the top albums in order" do
+      expect(Album.top).to match_array(@top_albums)
+    end
+
+    it "excludes lower ranked albums" do
+      expect(Album.top).to_not include(@album0)
+    end
+
   end
 
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validates incoming data" do
+    it "requires a name" do
+      movie = Movie.new
+
+      expect(movie).to_not be_valid
+      expect(movie.errors.keys).to include(:name)
+    end
+  end
+
+  describe "has top movies" do
+    before :each do
+      @movie0 = Movie.create(name: "zero", rank: 0)
+      movie1 = Movie.create(name: "one", rank: 1)
+      movie2 = Movie.create(name: "two", rank: 2)
+      movie3 = Movie.create(name: "three", rank: 3)
+
+      @top_movies = [movie3, movie2, movie1]
+    end
+
+
+    it "lists the top three movies" do
+      expect(Movie.top.count).to eq(3)
+    end
+
+    it "ranks the top movies in order" do
+      expect(Movie.top).to match_array(@top_movies)
+    end
+
+    it "excludes lower ranked movies" do
+      expect(Movie.top).to_not include(@movie0)
+    end
+
+  end
 end
