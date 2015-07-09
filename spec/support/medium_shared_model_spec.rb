6862452cda5b1844751892f71df888c3e53fe68f
Shanna Chau <shannachau@gmail.com>
2015-07-09 14:25:33 -0700

    Set up shared example for model rspec tests and they work

---
+++ spec/support/medium_shared_model_spec.rb
@@ -0,0 +1,33 @@
+RSpec.shared_examples "a medium model" do
+  let(:collection) { described_class.new }
+
+  describe "model validations" do
+    it "requires a name" do
+      expect(collection).to_not be_valid
+      expect(collection.errors.keys).to include(:name)
+    end
+
+    it "sets ranking to default of 0" do
+      expect(collection.ranking).to eq 0
+    end
+  end
+
+  describe "top scope" do
+    before :each do
+      @collection1 = described_class.create(name: 'whatever', ranking: 3)
+      @collection2 = described_class.create(name: 'whatever', ranking: 1)
+      @collection3 = described_class.create(name: 'whatevz',  ranking: 2)
+      @collection4 = described_class.create(name: 'whatevah', ranking: 4)
+    end
+
+    it "orders movies according to ranking, highest to lowest" do
+      top_collection = [@collection4, @collection1, @collection3]
+      expect(described_class.top(3)).to eq(top_collection)
+    end
+
+    it "only displays given number of movies" do
+      expect(described_class.top(3).count).to eq 3
+    end
+  end
+
+end
