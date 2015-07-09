f52d03e4c1733dae81481c33945255ef37e8f859
Anita <asovay@gmail.com>
2015-07-09 15:00:48 -0700

    shared examples complete for model specs

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 require 'support/shared_examples'
 
 RSpec.describe Album do
-  it_behaves_like "a book"
+  it_behaves_like "medium"
 end
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,40 +1,38 @@
-require 'set'
-
-RSpec.shared_examples "a book" do
+RSpec.shared_examples "medium" do 
   describe "model validations" do
     it "requires a name to be present" do
-      book = Book.new
+      media = described_class.new
 
-      expect(book).to_not be_valid
-      expect(book.errors.keys).to include(:name)
+      expect(media).to_not be_valid
+      expect(media.errors.keys).to include(:name)
     end
 
     it "requires a unique name" do
-      book1 = Book.create(name: "a book")
-      book2 = Book.create(name: "a book")
+      media1 = described_class.create(name: "a media")
+      media2 = described_class.create(name: "a media")
 
-      expect(book2).to_not be_valid
-      expect(book2.errors.keys).to include(:name)
+      expect(media2).to_not be_valid
+      expect(media2.errors.keys).to include(:name)
     end
 
     it "defaults votes to 0" do
-      book = Book.create(name: "name")
+      media = described_class.create(name: "name")
 
-      expect(book.vote).to eq 0
+      expect(media.vote).to eq 0
     end
   end
 
   describe ":best scope" do
     before :each do
-      @book4 = Book.create(name: "a book", vote: 8)
-      @book1 = Book.create(name: "b book", vote: 4)
-      @book3 = Book.create(name: "c book", vote: 19)
-      @book2 = Book.create(name: "d book", vote: 38)
+      @media4 = described_class.create(name: "a media", vote: 8)
+      @media1 = described_class.create(name: "b media", vote: 4)
+      @media3 = described_class.create(name: "c media", vote: 19)
+      @media2 = described_class.create(name: "d media", vote: 38)
     end
 
-    it "sorts the books in descending order by # of votes" do
-      correct_order = [@book2, @book3, @book4, @book1]
-      expect(Book.best(4)).to eq correct_order
+    it "sorts the medias in descending order by # of votes" do
+      correct_order = [@media2, @media3, @media4, @media1]
+      expect(described_class.best(4)).to eq correct_order
     end
   end
 end
