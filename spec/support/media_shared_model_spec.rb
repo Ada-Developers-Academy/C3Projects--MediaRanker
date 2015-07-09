9efe61a98d7d7c710bdbed0c09e4f6b5e6cf3105
Shanna Chau <shannachau@gmail.com>
2015-07-09 14:43:57 -0700

    Changed collection to media

--- spec/support/media_shared_model_spec.rb
+++ spec/support/media_shared_model_spec.rb
@@ -1,28 +1,28 @@
 RSpec.shared_examples "a media model" do
-  let(:collection) { described_class.new }
+  let(:media) { described_class.new }
 
   describe "model validations" do
     it "requires a name" do
-      expect(collection).to_not be_valid
-      expect(collection.errors.keys).to include(:name)
+      expect(media).to_not be_valid
+      expect(media.errors.keys).to include(:name)
     end
 
     it "sets ranking to default of 0" do
-      expect(collection.ranking).to eq 0
+      expect(media.ranking).to eq 0
     end
   end
 
   describe "top scope" do
     before :each do
-      @collection1 = described_class.create(name: 'whatever', ranking: 3)
-      @collection2 = described_class.create(name: 'whatever', ranking: 1)
-      @collection3 = described_class.create(name: 'whatevz',  ranking: 2)
-      @collection4 = described_class.create(name: 'whatevah', ranking: 4)
+      @media1 = described_class.create(name: 'whatever', ranking: 3)
+      @media2 = described_class.create(name: 'whatever', ranking: 1)
+      @media3 = described_class.create(name: 'whatevz',  ranking: 2)
+      @media4 = described_class.create(name: 'whatevah', ranking: 4)
     end
 
     it "orders movies according to ranking, highest to lowest" do
-      top_collection = [@collection4, @collection1, @collection3]
-      expect(described_class.top(3)).to eq(top_collection)
+      top_media = [@media4, @media1, @media3]
+      expect(described_class.top(3)).to eq(top_media)
     end
 
     it "only displays given number of movies" do
