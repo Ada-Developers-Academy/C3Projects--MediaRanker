b812c8b6940cc61ad8832b4c45b8ff9cef006a09
Sally Moore <sallyamoore@gmail.com>
2015-07-09 14:43:42 -0700

    Finished tests for MovieController

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,29 +3,32 @@ require 'rails_helper'
 RSpec.describe Movie, type: :model do
   # Testing Scopes:
   describe "scopes" do
-    let(:movie1) { Movie.create(title: "a title", rank: 56) }
-    let(:movie2) { Movie.create(title: "b title", rank: 6) }
-    let(:movie3) { Movie.create(title: "c title", rank: 10) }
-    let(:movie4) { Movie.create(title: "d title", rank: 500) }
-    let(:movie5) { Movie.create(title: "e title", rank: 3) }
-    let(:movie6) { Movie.create(title: "f title", rank: 23) }
+    before :each do
+      Movie.create(title: "a title", rank: 56)
+      Movie.create(title: "b title", rank: 6)
+      Movie.create(title: "c title", rank: 10)
+      Movie.create(title: "d title", rank: 500)
+      Movie.create(title: "e title", rank: 3)
+      Movie.create(title: "f title", rank: 23)
 
-    correct_order_top = [ 500, 56, 23, 10, 6 ]
-    order_top = []
-    Movie.all.top.each do |movie|
-      order_top << movie.rank
-    end
+      @correct_order_top = [ 500, 56, 23, 10, 6 ]
+      @order_top = []
+      Movie.all.top.each do |movie|
+        @order_top << movie.rank
+      end
 
-    correct_order_all = [ 500, 56, 23, 10, 6, 3 ]
-    order_all = []
-    Movie.all.rank_order.each do |movie|
-      order_all << movie.rank
+      @correct_order_all = [ 500, 56, 23, 10, 6, 3 ]
+      @order_all = []
+      Movie.all.rank_order.each do |movie|
+        @order_all << movie.rank
+      end
+
     end
 
     describe "top scope" do
       it "orders the top 5 records" do
         expect(Movie.top.count).to eq 5
-        expect(order_top).to eq correct_order_top
+        expect(@order_top).to eq @correct_order_top
       end
 
       it "does not include the 6th highest ranked" do
@@ -36,7 +39,7 @@ RSpec.describe Movie, type: :model do
     describe "rank_order scope" do
       it "orders all the records" do
         expect(Movie.rank_order.count).to eq 6
-        expect(order_all).to eq correct_order_all
+        expect(@order_all).to eq @correct_order_all
       end
     end
   end
