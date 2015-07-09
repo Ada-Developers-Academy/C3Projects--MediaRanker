7313d61bead24df906d18966554e66575f13ce15
Katie Atrops <katrops@gmail.com>
2015-07-08 22:45:10 -0700

    changed root_path so everything is in one controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'media#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
