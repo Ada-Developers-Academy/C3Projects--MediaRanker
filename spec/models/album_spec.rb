452354c6807fdf57d5c65c8555ef432b71f21e94
Elsa <elsa.moluf@gmail.com>
2015-07-08 17:05:17 -0700

    WIP tests.

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validates incoming data" do
+    it "requires a name" do
+      album = Album.new
+
+      expect(album).to_not be_valid
+      expect(album.errors.keys).to include(:title)
+    end
+  end
+
 end
