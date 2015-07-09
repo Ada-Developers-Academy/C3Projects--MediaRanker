047fabb3e64d38ce98bdc753f4ba3b0b2e0a3c18
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-09 16:22:54 -0700

    Adds model shared example specs and dries up model spec code

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,40 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Album, type: :model do
-  describe "model validations" do
-    it "requires a title, all the time" do
-      album = Album.new
-
-      expect(album).to_not be_valid
-      expect(album.errors.keys).to include(:title)
-    end
-  end
-
-  describe "best(total) scope" do
-    before :each do
-      @album1 = Album.create(title: "a", votes: 1)
-      @album2 = Album.create(title: "b", votes: 3)
-      @album3 = Album.create(title: "c", votes: 2)
-    end
-
-    # positive test - includes a number of highest ranked albums
-    it "includes a number of highest ranked albums" do
-      correct_order = [@album2, @album3]
-      expect(Album.best(2)).to eq correct_order
-    end
-
-    # negative test - excludes lower ranked albums
-    it "excludes lower ranked albums" do
-      expect(Album.best(2)).to_not include(@album1)
-    end
-  end
-
-  describe "#upvote(album)" do
-    it "increases the ranking" do
-      album = Album.create(title: "a", votes: 1)
-      Album.upvote(album)
-
-      expect(album.votes).to eq 2
-    end
+RSpec.describe Book, type: :model do
+  describe Book do
+    it_behaves_like "a medium"
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,40 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  describe "model validations" do
-    it "requires a title, all the time" do
-      book = Book.new
-
-      expect(book).to_not be_valid
-      expect(book.errors.keys).to include(:title)
-    end
-  end
-
-  describe "best(total) scope" do
-    before :each do
-      @book1 = Book.create(title: "a", votes: 1)
-      @book2 = Book.create(title: "b", votes: 3)
-      @book3 = Book.create(title: "c", votes: 2)
-    end
-
-    # positive test - includes a number of highest ranked books
-    it "includes a number of highest ranked books" do
-      correct_order = [@book2, @book3]
-      expect(Book.best(2)).to eq correct_order
-    end
-
-    # negative test - excludes lower ranked books
-    it "excludes lower ranked books" do
-      expect(Book.best(2)).to_not include(@book1)
-    end
-  end
-
-  describe "#upvote(book)" do
-    it "increases the ranking" do
-      book = Book.create(title: "a", votes: 1)
-      Book.upvote(book)
-
-      expect(book.votes).to eq 2
-    end
+  describe Book do
+    it_behaves_like "a medium"
   end
 end
