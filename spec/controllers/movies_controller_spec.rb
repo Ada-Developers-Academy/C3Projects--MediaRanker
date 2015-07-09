e0ba41bfad8020ba9ddc04cc4e724845e0b57161
Corinne Pingul <corinnepingul@gmail.com>
2015-07-09 16:31:04 -0700

    Got bootstrap setup and started styling the homepage.

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -15,6 +15,8 @@ RSpec.describe MoviesController, type: :controller do
       @movie = Movie.create(title: "Harry Potter", rank: 0)
     end
 
+    let(:description_params) { { id: @movie.id, movie: { description: "yes" } } }
+
     # positive test - when upvote is pressed, it only updates the rank
     it "updates the rank by one when upvote is pressed" do
       put :update, id: @movie.id
@@ -24,11 +26,22 @@ RSpec.describe MoviesController, type: :controller do
 
     # positive test - when form is edited, it updates the form information
     it "updates the description when you edit the form" do
-      put :update, id: @movie.id, movie: { description: "yes" }
+      put :update, description_params
 
       expect(Movie.find(@movie.id).description).to eq "yes"
     end
+
     # when it updates the form information, the upvote isn't changed
+    it "doesn't change the rank when you update the description" do
+      put :update, description_params
+
+      expect(Movie.find(@movie.id).rank).to eq 0
+    end
+
     # it redirects to the show page
+    it "redirects to the movie show page" do
+      put :update, description_params
+      expect(subject).to render(movie_path)
+    end
   end
 end
