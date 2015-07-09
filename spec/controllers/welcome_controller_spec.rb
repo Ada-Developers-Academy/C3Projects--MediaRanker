3ddfc0f695906710c989a392074901dee48d7c55
Kari <kari.lonczak@gmail.com>
2015-07-08 18:02:20 -0700

    Created controller specs. Started writing tests for them.

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+
+  describe "GET index" do
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "assigns @movies to all of the movies in the database" do
+      movie1 = Movie.new
+      movie2 = Movie.new
+      movie3 = Movie.new
+
+      @movies = Movie.all
+
+      get :index
+      expect(@movies.length).to eq(3)
+    end
+  end
+
+end
