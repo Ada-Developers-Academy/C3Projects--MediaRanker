3ebccf02a818a9670614dde3b815fd0f19f202f0
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-09 00:02:07 -0700

    created tests for Movie scopes :ranked and :top_ranked

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -20,4 +20,59 @@ RSpec.describe Movie, type: :model do
       end
     end
   end
+
+    describe "ranked scope" do
+    # positive test
+    it "orders the movies descending by rank" do
+      medium1 = Movie.create(title: 'a', rank: 1)
+      medium2 = Movie.create(title: 'b', rank: 2)
+      medium3 = Movie.create(title: 'c', rank: 3)
+
+      expect(Movie.ranked.first).to eq medium3
+    end
+
+    # negative test
+    it "does not order the movies by id" do
+      medium1 = Movie.create(title: 'a', rank: 1)
+      medium2 = Movie.create(title: 'b', rank: 2)
+      medium3 = Movie.create(title: 'c', rank: 3)
+
+      expect(Movie.ranked.first).to_not eq Movie.find(1)
+    end
+  end
+
+  describe "top_ranked scope" do
+    before :each do
+      @medium1 = Movie.create(title: 'a', rank: 1)
+      @medium2 = Movie.create(title: 'b', rank: 2)
+      @medium3 = Movie.create(title: 'c', rank: 3)
+      @medium4 = Movie.create(title: 'd', rank: 4)
+      @medium5 = Movie.create(title: 'e', rank: 5)
+      @medium6 = Movie.create(title: 'f', rank: 6)
+      @medium7 = Movie.create(title: 'g', rank: 7)
+      @medium8 = Movie.create(title: 'h', rank: 8)
+      @medium9 = Movie.create(title: 'i', rank: 9)
+      @medium10 = Movie.create(title: 'j', rank: 10)
+      @medium11 = Movie.create(title: 'k', rank: 11)
+      @medium12 = Movie.create(title: 'l', rank: 12)
+    end
+
+    # positive test
+    it "orders the movies descending by rank" do
+      expect(Movie.top_ranked.first).to eq @medium12
+    end
+
+    it "only has 10 elements" do
+      expect(Movie.top_ranked.count).to eq 10
+    end
+
+    # negative test
+    it "does not order the movies by id" do
+      expect(Movie.top_ranked.first).to_not eq Movie.find(1)
+    end
+
+    it "does not include the lowest ranking movies" do
+      expect(Movie.top_ranked).to_not include @medium1
+    end
+  end
 end
