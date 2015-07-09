cf402ff90e4e73aa227134f2ddc12f4a40674684
Loren Wang <wang131.131@gmail.com>
2015-07-08 19:40:59 -0700

    updated models' scopes

--- app/models/album.rb
+++ app/models/album.rb
@@ -7,6 +7,7 @@ class Album < ActiveRecord::Base
    # Scopes ---------------------------------------------------------------------
 
   scope :top, -> { order('rank DESC').limit(5) }
+  scope :ordered, -> { order('rank DESC') }
   #  scope :on, -> (label) { where(label: label) }
   #  scope :available_formats, -> { select(:format).distinct.order(:format).pluck(:format) }
 
