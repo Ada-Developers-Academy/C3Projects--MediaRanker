a161f3cb7ae40435a8c45e9cc0a5d10eca1daa10
Jeri <drvonnjerryxlii@gmail.com>
2015-07-08 19:37:05 -0700

    what have i done

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -8,13 +8,16 @@ class Medium < ActiveRecord::Base
 
   # sets the fancy words for each category
   CATEGORIES = [
-    books: { name: "Book", creator_noun: "Author", created: "Written" },
-    movies: { name: "Movie", creator_noun: "Director", created: "Directed" },
-    albums: { name: "Album", creator_noun: "Artist", created: "Produced" }
+    books: { name: "Book", plural: "books", creator_noun: "Author", created: "Written" },
+    movies: { name: "Movie", plural: "movies", creator_noun: "Director", created: "Directed" },
+    albums: { name: "Album", plural: "albums", creator_noun: "Artist", created: "Produced" }
   ]
 
-  CATEGORIES.each do |category_symbol, hash|
-    scope category_symbol, -> { where(category: hash[:name]) }
+  CATEGORIES.each do |category_hash|
+    category_hash.each do |category, inner_hash|
+      # scope :available_formats, -> { select(:format).distinct.order(:format).pluck(:format) }
+      scope category, -> { where(category: inner_hash[:name]) }
+    end
   end
 
   def plural_category
@@ -36,9 +39,11 @@ class Medium < ActiveRecord::Base
   def self.categorize
     all_records = []
 
-    CATEGORIES.each do |category, _value|
-      first_ten_records = self.grab_category(category.to_s).limit(10)
-      all_records.push(first_ten_records) if first_ten_records.length > 0
+    CATEGORIES.each do |category_hash|
+      category_hash.each do |category, inner_hash|
+        first_ten_records = self.grab_category(inner_hash[:plural]).limit(10)
+        all_records.push(first_ten_records) if first_ten_records.length > 0
+      end
     end
 
     all_records
