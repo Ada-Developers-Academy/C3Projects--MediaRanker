ffb5fdd7652578e63b27ad1d104fb8ffa9b5439e
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-09 16:43:14 -0700

    added shared_examples for models; converted Album, Book, and Movie specs to use shared_examples

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,78 +1,8 @@
 require 'rails_helper'
+require 'support/shared_examples_for_media'
 
 RSpec.describe Book, type: :model do
-  describe "model validations" do
-    it "requires a title, all the time" do
-      medium = Book.new
-
-      expect(medium).to_not be_valid
-      expect(medium.errors.keys).to include(:title)
-    end
-
-    context "validating rank" do
-      ["pork", 3.14, -1].each do |invalid_rank|
-        it "doesn't validate #{invalid_rank} for rank" do
-          medium = Book.new(rank: invalid_rank)
-
-          expect(medium).to_not be_valid
-          expect(medium.errors.keys).to include(:rank)
-        end
-      end
-    end
-  end
-
-  describe "ranked scope" do
-    # positive test
-    it "orders the books descending by rank" do
-      medium1 = Book.create(title: 'a', rank: 1)
-      medium2 = Book.create(title: 'b', rank: 2)
-      medium3 = Book.create(title: 'c', rank: 3)
-
-      expect(Book.ranked.first).to eq medium3
-    end
-
-    # negative test
-    it "does not order the books by id" do
-      medium1 = Book.create(title: 'a', rank: 1)
-      medium2 = Book.create(title: 'b', rank: 2)
-      medium3 = Book.create(title: 'c', rank: 3)
-
-      expect(Book.ranked.first).to_not eq Book.find(1)
-    end
-  end
-
-  describe "top_ranked scope" do
-    before :each do
-      @medium1 = Book.create(title: 'a', rank: 1)
-      @medium2 = Book.create(title: 'b', rank: 2)
-      @medium3 = Book.create(title: 'c', rank: 3)
-      @medium4 = Book.create(title: 'd', rank: 4)
-      @medium5 = Book.create(title: 'e', rank: 5)
-      @medium6 = Book.create(title: 'f', rank: 6)
-      @medium7 = Book.create(title: 'g', rank: 7)
-      @medium8 = Book.create(title: 'h', rank: 8)
-      @medium9 = Book.create(title: 'i', rank: 9)
-      @medium10 = Book.create(title: 'j', rank: 10)
-      @medium11 = Book.create(title: 'k', rank: 11)
-      @medium12 = Book.create(title: 'l', rank: 12)
-    end
-
-    # positive test
-    it "orders the books descending by rank" do
-      expect(Book.top_ranked.first).to eq @medium12
-    end
-
-    it "only has 10 elements" do
-      expect(Book.top_ranked.count).to eq 10
-    end
-
-    # negative test
-    it "does not order the books by id" do
-      expect(Book.top_ranked.first).to_not eq Book.find(1)
-    end
-
-    it "does not include the lowest ranking books" do
-      expect(Book.top_ranked).to_not include @medium1
-    end
+  describe Book do
+    it_behaves_like "a medium"
   end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,78 +1,8 @@
 require 'rails_helper'
+require 'support/shared_examples_for_media'
 
 RSpec.describe Movie, type: :model do
-  describe "model validations" do
-    it "requires a title, all the time" do
-      medium = Movie.new
-
-      expect(medium).to_not be_valid
-      expect(medium.errors.keys).to include(:title)
-    end
-
-    context "validating rank" do
-      ["pork", 3.14, -1].each do |invalid_rank|
-        it "doesn't validate #{invalid_rank} for rank" do
-          medium = Movie.new(rank: invalid_rank)
-
-          expect(medium).to_not be_valid
-          expect(medium.errors.keys).to include(:rank)
-        end
-      end
-    end
-  end
-
-    describe "ranked scope" do
-    # positive test
-    it "orders the movies descending by rank" do
-      medium1 = Movie.create(title: 'a', rank: 1)
-      medium2 = Movie.create(title: 'b', rank: 2)
-      medium3 = Movie.create(title: 'c', rank: 3)
-
-      expect(Movie.ranked.first).to eq medium3
-    end
-
-    # negative test
-    it "does not order the movies by id" do
-      medium1 = Movie.create(title: 'a', rank: 1)
-      medium2 = Movie.create(title: 'b', rank: 2)
-      medium3 = Movie.create(title: 'c', rank: 3)
-
-      expect(Movie.ranked.first).to_not eq Movie.find(1)
-    end
-  end
-
-  describe "top_ranked scope" do
-    before :each do
-      @medium1 = Movie.create(title: 'a', rank: 1)
-      @medium2 = Movie.create(title: 'b', rank: 2)
-      @medium3 = Movie.create(title: 'c', rank: 3)
-      @medium4 = Movie.create(title: 'd', rank: 4)
-      @medium5 = Movie.create(title: 'e', rank: 5)
-      @medium6 = Movie.create(title: 'f', rank: 6)
-      @medium7 = Movie.create(title: 'g', rank: 7)
-      @medium8 = Movie.create(title: 'h', rank: 8)
-      @medium9 = Movie.create(title: 'i', rank: 9)
-      @medium10 = Movie.create(title: 'j', rank: 10)
-      @medium11 = Movie.create(title: 'k', rank: 11)
-      @medium12 = Movie.create(title: 'l', rank: 12)
-    end
-
-    # positive test
-    it "orders the movies descending by rank" do
-      expect(Movie.top_ranked.first).to eq @medium12
-    end
-
-    it "only has 10 elements" do
-      expect(Movie.top_ranked.count).to eq 10
-    end
-
-    # negative test
-    it "does not order the movies by id" do
-      expect(Movie.top_ranked.first).to_not eq Movie.find(1)
-    end
-
-    it "does not include the lowest ranking movies" do
-      expect(Movie.top_ranked).to_not include @medium1
-    end
+  describe Movie do
+    it_behaves_like "a medium"
   end
 end
