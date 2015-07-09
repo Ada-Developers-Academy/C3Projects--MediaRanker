a155e8e0b2e71078a4418e18c4facca8e4b11478
Ashley Watkins <awatkins31@gmail.com>
2015-07-08 18:01:50 -0700

    Adds functionality to create a new movie record

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,15 +9,36 @@ class MoviesController < ApplicationController
   # def show
   # end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    if movie = Movie.create(movie_params)
+      redirect_to movie_path(movie)
+    else
+      #TODO: something
+    end
+  end
+
   def update
     if params[:upvote] == "true"
       Movie.upvote(@movie)
       redirect_to movie_path(@movie)
+    elsif @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      #TODO: add error message
+      render :edit
     end
   end
 
   private
   def set_movie
     @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :directed_by, :description)
   end
 end
