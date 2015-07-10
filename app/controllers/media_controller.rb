7f86a5e2ecfd6dde6dc07c2fd42f63178019e994
Loraine <loraine@lorainekv.com>
2015-07-10 14:02:09 -0700

    Last minute comments.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,6 +1,8 @@
 class MediaController < ApplicationController
   def index
+    #In case my bosses at MediaRanker want to change it to a top 20 list
     top_num = 10
+
     @movies = Medium.movies.top_rank(top_num)
 
     @books = Medium.books.top_rank(top_num)
@@ -14,6 +16,7 @@ class MediaController < ApplicationController
     render :all_media
   end
 
+  # This seems redundant. However, all these methods do something different.
   def all_books
     @books = Medium.books.all_rank
 
@@ -29,6 +32,7 @@ class MediaController < ApplicationController
   def show
     @medium = Medium.find(params[:id])
 
+    # The @type helps define which _media_details locals to load into the view
     @type = @medium.media_type.pluralize
   end
 
@@ -41,7 +45,7 @@ class MediaController < ApplicationController
 
     if @medium.save
       redirect_to medium_path(@medium)
-    else
+    else # guard clause
       redirect_to new_medium_path(@medium.media_type)
     end
 
