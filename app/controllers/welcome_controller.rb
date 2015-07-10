684be790da953cf6b2656ec9db0dd49746f289af
Lila Garcia <lilagrc@gmail.com>
2015-07-10 15:50:51 -0700

    couple last minute changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    @movies = Movie.ranking(5)
-    @books = Book.ranking(5)
-    @albums = Album.ranking(5)
+    @movies = Movie.ranking(10)
+    @books = Book.ranking(10)
+    @albums = Album.ranking(10)
   end
 
 end
