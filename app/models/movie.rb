b82a5a13886571142c8234bf2d4d8a493da9b54a
Loren Wang <wang131.131@gmail.com>
2015-07-10 12:56:36 -0700

    edited movie model...don't remember. vauge, i know

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,7 +7,7 @@ class Movie < ActiveRecord::Base
    # Scopes ---------------------------------------------------------------------
   scope :top, -> { order('rank DESC').limit(10) }
   scope :ordered, -> { order('rank DESC') }
-    #
+    
     # scope :desc, order("event_at DESC")
   #  scope :available_formats, -> { select(:format).distinct.order(:format).pluck(:format) }
 
