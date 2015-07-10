aa1b005c8ae0866a178481d99a762b92f2376650
Ashley Watkins <awatkins31@gmail.com>
2015-07-10 09:16:26 -0700

    Switch from 3 controllers to 1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :movies
-  resources :books
-  resources :albums
+  resources :media, only: [:create, :update]
+
+  resources :movies, controller: "media"
+  resources :books, controller: "media"
+  resources :albums, controller: "media"
+
 end
