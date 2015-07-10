20a10221a33901200a76b0862ce9bbbacbb19f5f
Anita <asovay@gmail.com>
2015-07-10 13:26:49 -0700

    shared examples controllers WIP

--- spec/support/shared_controller_examples.rb
+++ spec/support/shared_controller_examples.rb
@@ -1,136 +1,113 @@
-RSpec.shared_examples "MediaController", type: :controller do
-  describe "GET #index" do
-    it "responds successfully with an HTTP 200 status code" do
-      get :index
-
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-    end
-  end
-
-  describe "GET #new" do
-    let (:media_params) do 
-      { 
-        media: { name: "name" }
-      }
-    end
-
-    it "creates a new instance of Media" do
-      get :new, media_params
-      expect(assigns(:media)).to be_kind_of(described_class.model)
-    end
-  end
-
-  describe "POST #create" do
-    context "valid media params" do
-      let(:media_params) do
-        {
-          media: {
-            name: "title",
-            author: "author",
-            desc: "desc",
-            vote: 2
-          }
-        }
-      end
-
-      it "creates a Media record" do
-        post :create, media_params
-        expect(described_class.model.count).to eq 1
-      end
-
-      it "redirects to the Media show page" do
-        post :create, media_params
-        expect(subject).to redirect_to(media_path(assigns(:media)))
+RSpec.shared_examples "MediaController" do
+    describe "GET #index" do
+      it "responds successfully with an HTTP 200 status code" do
+        get :index
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
       end
     end
 
-    context "invalid media params" do
-      let (:media_params) do
-        {
-          media: { 
-            author: "author",
-            desc: "desc",
-            vote: 2
-          }
-        }
-      end
-
-      it "does not persist invalid records" do
-        post :create, media_params
-        expect(described_class.model.count).to eq 0
-      end
-
-      it "renders the :new view (to allow users to fix invalid data)" do
-        post :create, media_params
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PATCH #update" do
-    before :each do 
-      @media = described_class.model.create(name: "name")
-    end
-    
-    context "valid media params" do
-      it "updates an existing media record" do
-        patch :update, id: @media.id, media: { name: "title" }
-        @media.reload
-        expect(@media.name).to eq "title"
-      end
-
-      it "redirects to the Media show page" do
-        patch :update, id: @media.id, media: { name: "title" }
-        @media.reload
-        expect(subject).to redirect_to(media_path)
+    describe "GET #new" do
+      it "creates a new instance of Media" do
+        get :new, @media_params
+        expect(assigns(@media)).to be_kind_of(@model)
       end
     end
 
-    context "invalid media params" do
-      it "does not persist invalid record updates" do
-        patch :update, id: @media.id, media: { desc: "desc" }
-        expect(subject).to redirect_to(media_path)
-      end
-    end
-  end
+    describe "POST #create" do
 
-  describe "POST #upvote" do
-    before :each do 
-      @media = described_class.model.create(name: "name", vote: 0)
-    end
+      context "valid media params" do
+        it "creates a Media record" do
+          post :create, @media_params
+          expect(@model.count).to eq 1
+        end
 
-    context "clicking upvote button" do
-      it "increases the upvote count by 1" do
-        post :upvote, id: @media.id
-        @media.reload
-        expect(@media.vote).to eq 1
+        it "redirects to the show page" do
+          post :create, @media_params
+          expect(subject).to redirect_to(book_path(assigns(@media)))
+        end
       end
 
-      it "redirects to the Media show page" do
-        patch :upvote, id: @media.id
-        @media.reload
-        expect(subject).to redirect_to(media_path)
+      context "invalid media params" do
+
+        it "does not persist invalid records" do
+          post :create, @invalid_media_params
+          expect(@model.count).to eq 0
+        end
+
+        it "renders the :new view (to allow users to fix invalid data)" do
+          post :create, @invalid_media_params
+          expect(response).to render_template(:new)
+        end
+      end
+    end
+
+    describe "PATCH #update" do
+      before :each do 
+        @created = @model.create(name: "name")
+      end
+      
+      context "valid params" do
+        it "updates an existing record" do
+          # { book: { name: "title" }}
+          patch :update, { id: @created.id }.merge(new_params)
+          # put :update, id: @created.id, { book: { name: "title" }}
+          # put :update, { id: @created.id, book: { name: "title" } }
+          @created.reload
+          expect(@created.name).to eq "title"
+        end
+
+        it "redirects to the show page" do
+          patch :update, { id: @created.id }.merge(new_params)
+          @created.reload
+          expect(subject).to redirect_to(book_path)
+        end
+      end
+
+      context "invalid params" do
+        it "does not persist invalid record updates" do
+          patch :update, { id: @created.id }.merge(@invalid_media_params)
+          expect(subject).to redirect_to(book_path)
+        end
+      end
+    end
+
+    describe "POST #upvote" do
+      before :each do 
+        @model = @model.create(name: "name", vote: 0)
+      end
+
+      context "clicking upvote button" do
+        it "increases the upvote count by 1" do
+          post :upvote, id: @model.id
+          @model.reload
+          expect(@model.vote).to eq 1
+        end
+
+        it "redirects to the show page" do
+          patch :upvote, id: @model.id
+          @model.reload
+          expect(subject).to redirect_to(book_path)
+        end
+      end
+    end
+
+    describe "DELETE #destroy" do
+      before :each do 
+        @delete = @model.create(name: "name")
+      end
+
+      context "clicking the delete button" do
+        it "removes a record" do
+          expect {
+            delete :destroy, {:id => @delete.id}
+            }.to change(@model, :count).by(-1)
+        end
+
+        it "redirects to the show page" do
+          delete :destroy, id: @delete.id
+          expect(subject).to redirect_to(books_path)
+        end
       end
     end
   end
-
-  describe "DELETE #destroy" do
-    before :each do 
-      @media = described_class.model.create(name: "name")
-    end
-
-    context "clicking the delete button" do
-      it "removes a record" do
-        expect {
-          delete :destroy, {:id => @media.id}
-          }.to change(described_class.model, :count).by(-1)
-      end
-
-      it "redirects to the Media show page" do
-        delete :destroy, id: @media.id
-        expect(subject).to redirect_to(medias_path)
-      end
-    end
-  end
-end
