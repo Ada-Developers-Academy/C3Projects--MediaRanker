27b47f721c88607067450de271ea606dd5e2c46b
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-10 09:35:52 -0700

    cleaned up routes; using member route for #upvote

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  put '/movies/:id/upvote' => 'movies#upvote', as: 'upvote_movie'
-  put '/books/:id/upvote' => 'books#upvote', as: 'upvote_book'
-  put '/albums/:id/upvote' => 'albums#upvote', as: 'upvote_album'
-
-  resources :movies
-  resources :books
-  resources :albums
-
-  # try doing for upvote paths
-  # member do
-  #   patch 'upvote'
-  # end
+  resources :movies, :books, :albums do
+    member do
+      put 'upvote'
+    end
+  end
 end
