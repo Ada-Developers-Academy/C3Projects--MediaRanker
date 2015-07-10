a473946a600cb3de3ba15d403247d239dd169a9f
Loraine <loraine@lorainekv.com>
2015-07-10 13:45:49 -0700

    Style guide tweaks.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,27 +1,33 @@
 class MediaController < ApplicationController
   def index
     @movies = Medium.movies.top_rank(10)
+
     @books = Medium.books.top_rank(10)
+
     @albums = Medium.albums.top_rank(10)
   end
 
   def all_movies
     @movies = Medium.movies.all_rank
+
     render :all_media
   end
 
   def all_books
     @books = Medium.books.all_rank
+
     render :all_media
   end
 
   def all_albums
     @albums = Medium.albums.all_rank
+
     render :all_media
   end
 
   def show
     @medium = Medium.find(params[:id])
+
     @type = @medium.media_type.pluralize
   end
 
@@ -31,17 +37,22 @@ class MediaController < ApplicationController
 
   def create
     @medium = Medium.new(create_params[:medium])
+
     if @medium.save
       redirect_to medium_path(@medium)
     else
       redirect_to new_medium_path(@medium.media_type)
     end
+
   end
 
   def upvote
     @medium = Medium.find(params[:id])
+
     @medium.ranking += 1
+
     @medium.save
+
     redirect_to medium_path
   end
 
@@ -61,8 +72,6 @@ class MediaController < ApplicationController
     @medium = Medium.destroy(params[:id])
 
     redirect_to "/#{params[:format]}/index"
-
-
   end
 
   private
--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -3,27 +3,29 @@ require 'rails_helper'
 RSpec.describe MediaController, type: :controller do
 
   describe "GET #index" do
-
     it "renders the index template" do
       get :index
+
       expect(response).to render_template("index")
     end
 
     media_types = { book: "books", movie: "movies", album: "albums" }
+
     media_types.each do |type, instance_variable|
+
       it "loads media into @#{instance_variable}" do
         medium1 = Medium.create!(name: "new1", media_type: type)
+
         medium2 = Medium.create!(name: "new2", media_type: type)
+
         get :index
 
         expect(assigns(:"#{instance_variable}")).to match_array([medium1, medium2])
       end
     end
-
   end
 
   describe "GET #show" do
-
     before(:each) do
       @medium = Medium.create!(id: 5, name: "new medium", media_type: "book")
     end
@@ -36,13 +38,13 @@ RSpec.describe MediaController, type: :controller do
 
     it "renders the show template" do
       get :show, id: @medium
+
       expect(response).to render_template :show
     end
 
   end
 
   describe "POST #create" do
-    #positive test - when the media params are valid
     context "Valid media params" do
       let (:medium_params) do
         {
@@ -59,7 +61,9 @@ RSpec.describe MediaController, type: :controller do
 
       it "creates an media record" do
         post :create, medium_params
+
         expect(Medium.count).to eq(1)
+
         expect(response).to redirect_to(assigns(:medium))
       end
     end
@@ -72,12 +76,16 @@ RSpec.describe MediaController, type: :controller do
 
     before(:each) do
       @medium = Medium.create!(ranking: "4", name: "Book name", media_type: "book")
+
       put :update, :id => @medium.id, :medium => attr
+
       @medium.reload
     end
 
     it { expect(response).to redirect_to(@medium) }
+
     it { expect(@medium.name).to eq attr[:name] }
+
     it { expect(@medium.description).to eq attr[:description] }
   end
 
@@ -85,9 +93,9 @@ RSpec.describe MediaController, type: :controller do
 
     context "when deletion succeeds" do
 
-
       it "deletes a medium record" do
         @medium = Medium.create!(ranking: "4", name: "Book name", media_type: "book")
+
         expect { delete :destroy, :id => @medium.id }.to change(Medium, :count).by(-1)
       end
     end
@@ -96,10 +104,12 @@ RSpec.describe MediaController, type: :controller do
   describe "upboat" do
     it "increases ranking when upboat method is used" do
       @medium = Medium.create!(name: "Book name", media_type: "book")
+
       patch :upvote, :id => @medium.id
+
       @medium.reload
-      expect(@medium.ranking).to eq 1
+
+      expect(@medium.ranking).to eq(1)
     end
   end
-
 end
