9e91d4cdb7b493c5df181ae7fd48a3d73494b526
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-09 17:54:26 -0700

    added TOP_NUMBER constant to control number of top_ranked records

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,8 +4,10 @@ class Movie < ActiveRecord::Base
   validates :rank, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   # Scopes ---------------------------------------------------------------------
+  TOP_NUMBER = 10
+
   scope :ranked,      -> { order(rank: :desc) }
-  scope :top_ranked,  -> { ranked.limit(10) }
+  scope :top_ranked,  -> { ranked.limit(TOP_NUMBER) }
 
   def add_vote
     update(rank: self.rank + 1)
