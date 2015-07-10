7ea192b5d6d77c4f62bd3e52e7ad19d7ffa56093
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-10 14:08:57 -0700

    Finished model shared rspecs.

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like "a media"
+
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like "a media"
+
 end
