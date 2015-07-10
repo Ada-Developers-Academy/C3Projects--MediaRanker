9e212878abdba48f3b59339baf860996187517d9
Sally Moore <sallyamoore@gmail.com>
2015-07-10 11:58:35 -0700

    Fixed the shared rspec controller and now IT WORKS. :) Updated movies controller spec. Now to add controller specs for books and albums that use the shared spec. YAY

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,154 +2,23 @@ require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
   it_behaves_like "a medium controller"
-
-  describe "GET #index" do
-    it "responds successfully with an HTTP 200 status code" do
-      get :index
-
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-    end
+  let(:params) do
+    {
+      movie: {
+        title: "a title",
+        director: "a director",
+        description: "a description"
+      }
+    }
   end
 
-  describe "POST #create" do
-    context "valid movie params" do
-      let(:movie_params) do
-        {
-          movie: {
-            title: "a title",
-            director: "an artist",
-            rank: 20,
-            description: "a description"
-          }
-        }
-      end
-
-      it "creates a Movie record" do
-        post :create, movie_params
-        expect(Movie.count).to eq 1
-      end
-
-      it "redirects to the movie show page" do
-        post :create, movie_params
-        expect(subject).to redirect_to(movie_path(assigns(:movie)))
-      end
-
-      context "record in which only the title is specified" do
-        let(:minimal_movie_params) do
-          { movie: { title: "a title" } }
-        end
-
-        it "creates a Movie record" do
-          post :create, minimal_movie_params
-          expect(Movie.count).to eq 1
-        end
-
-        it "sets value of rank to 0" do
-          post :create, minimal_movie_params
-          expect(Movie.first.rank).to eq 0
-        end
-
-        it "redirects to the movie show page" do
-          post :create, minimal_movie_params
-          expect(subject).to redirect_to(movie_path(assigns(:movie)))
-        end
-      end
-    end
-
-    context "invalid movie params" do
-      let(:movie_params) do
-        { movie: { director: "a director" } }
-      end
-
-      it "does not persist a Movie record" do
-        post :create, movie_params
-        expect(Movie.count).to eq 0
-      end
-
-      it "renders the :new view (to allow users to fix invalid data)" do
-        post :create, movie_params
-        expect(response).to render_template("new")
-      end
-    end
+  let(:minimal_params) do
+    { movie: { title: "a title" } }
   end
 
-  describe "PUT update/:id" do
-    let(:attr) do
-      { title: "new title",
-        director: "new director",
-        description: "new description" }
-    end
-
-    before :each do
-      @movie = Movie.create(title: "a title", director: "an artist", rank: 20, description: "a description")
-      put :update, :id => @movie.id, :movie => attr
-      @movie.reload
-    end
-
-    it "updates the movie record" do
-      expect(response).to redirect_to(@movie)
-      expect(@movie.title).to eq "new title"
-      expect(@movie.director).to eq "new director"
-      expect(@movie.description).to eq "new description"
-    end
-
-    it "redirects to the movie show page" do
-      expect(subject).to redirect_to(movie_path(assigns(:movie)))
-    end
+  let(:invalid_params) do
+    { movie: { description: "a description" } }
   end
 
-
-  describe "GET #show" do
-    before :each do
-      @movie = Movie.create(title: "a title", director: "an artist", rank: 20, description: "a description")
-    end
-
-    it "shows the selected movie" do
-      get :show, id: @movie
-
-      expect { assigns(:movie).to eq(@movie) }
-    end
-
-    it  "renders the #show template" do
-      get :show, id: @movie.id
-      expect(subject).to render_template :show
-    end
-  end
-
-  describe "DELETE #destroy" do
-    before :each do
-      @movie = Movie.create(title: "a title", director: "an artist", rank: 20, description: "a description")
-    end
-
-    it "deletes the record" do
-      expect{
-        delete :destroy, id: @movie
-        }.to change(Movie, :count).by(-1)
-    end
-
-    it "redirects to movies_path" do
-      delete :destroy, id: @movie
-      expect(response).to redirect_to movies_path
-    end
-  end
-
-  describe "PATCH #upvote" do
-    before :each do
-      @movie = Movie.create(title: "a title", director: "an artist", rank: 20, description: "a description")
-      patch :upvote, id: @movie
-      @movie.reload
-    end
-
-    it "increases the rank by 1" do
-      expect(@movie.rank).to eq 21
-    end
-
-    it "redirects to movies_path" do
-      patch :upvote, id: @movie
-      expect(response).to redirect_to movie_path(@movie.id)
-    end
-  end
-
-
+  let(:medium_name) { :movie }
 end
--- spec/support/medium_controller_spec.rb
+++ spec/support/medium_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 RSpec.shared_examples "a medium controller" do
+
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
       get :index
@@ -11,140 +12,119 @@ RSpec.shared_examples "a medium controller" do
   end
 
   describe "POST #create" do
-    context "valid #{described_class.model} params" do
-      let(:params) do
-        {
-          medium: {
-            title: "a title",
-            rank: 20,
-            description: "a description"
-          }
-        }
-      end
+    let(:medium) { described_class.model.first }
 
+    context "valid #{described_class.model} params" do
       it "creates a #{described_class.model} record" do
         post :create, params
         expect(described_class.model.count).to eq 1
       end
 
+      it "sets value of rank to 0" do
+        post :create, params
+        expect(medium.rank).to eq 0
+      end
+
       it "redirects to the show page" do
         post :create, params
-        expect(subject).to redirect_to(polymorphic_path(described_class))
+        expect(subject).to redirect_to(polymorphic_path(medium))
       end
 
       context "record in which only the title is specified" do
-        let(:params) do
-          { medium: { title: "a title" } }
-        end
-
         it "creates a record" do
-          post :create, params
+          post :create, minimal_params
           expect(described_class.model.count).to eq 1
-        end
-
-        it "sets value of rank to 0" do
-          post :create, params
-          expect(described_class.model.first.rank).to eq 0
-        end
-
-        it "redirects to the medium show page" do
-          post :create, params
-          expect(subject).to redirect_to(polymorphic_path(assigns(:medium)))
         end
       end
     end
 
     context "invalid params" do
-      let(:params) do
-        { medium: { description: "a description" } }
-      end
-
       it "does not persist a #{described_class.model} record" do
-        post :create, params
+        post :create, invalid_params
         expect(described_class.model.count).to eq 0
       end
 
       it "renders the :new view (to allow users to fix invalid data)" do
-        post :create, params
+        post :create, invalid_params
         expect(response).to render_template("new")
       end
     end
   end
-  #
-  # describe "PUT update/:id" do
-  #   let(:attr) do
-  #     { title: "new title",
-  #       director: "new director",
-  #       description: "new description" }
-  #   end
-  #
-  #   before :each do
-  #     @medium = described_class.model.create(title: "a title", director: "an artist", rank: 20, description: "a description")
-  #     put :update, :id => @medium.id, :medium => attr
-  #     @medium.reload
-  #   end
-  #
-  #   it "updates the medium record" do
-  #     expect(response).to redirect_to(@medium)
-  #     expect(@medium.title).to eq "new title"
-  #     expect(@medium.director).to eq "new director"
-  #     expect(@medium.description).to eq "new description"
-  #   end
-  #
-  #   it "redirects to the medium show page" do
-  #     expect(subject).to redirect_to(polymorphic_path(assigns(:medium)))
-  #   end
-  # end
-  #
-  #
-  # describe "GET #show" do
-  #   before :each do
-  #     @medium = described_class.model.create(title: "a title", director: "an artist", rank: 20, description: "a description")
-  #   end
-  #
-  #   it "shows the selected medium" do
-  #     get :show, id: @medium
-  #
-  #     expect { assigns(:medium).to eq(@medium) }
-  #   end
-  #
-  #   it  "renders the #show template" do
-  #     get :show, id: @medium.id
-  #     expect(subject).to render_template :show
-  #   end
-  # end
-  #
-  # describe "DELETE #destroy" do
-  #   before :each do
-  #     @medium = described_class.model.create(title: "a title", director: "an artist", rank: 20, description: "a description")
-  #   end
-  #
-  #   it "deletes the record" do
-  #     expect{
-  #       delete :destroy, id: @medium
-  #     }.to change(described_class.model, :count).by(-1)
-  #   end
-  #
-  #   it "redirects to #{described_class.model}s_path" do
-  #     delete :destroy, id: @medium
-  #     expect(response).to redirect_to polymorphic_path("#{@medium.downcase}s")
-  #   end
-  # end
-  #
-  # describe "PATCH #upvote" do
-  #   before :each do
-  #     @medium = described_class.model.create(title: "a title", director: "an artist", rank: 20, description: "a description")
-  #     patch :upvote, id: @medium
-  #     @medium.reload
-  #   end
-  #
-  #   it "increases the rank by 1" do
-  #     expect(@medium.rank).to eq 21
-  #   end
-  #
-  #   it "redirects to #{described_class.model}_path" do
-  #     patch :upvote, id: @medium
-  #     expect(response).to redirect_to polymorphic_path(@medium.id)
-  #   end
-  # end
+
+  describe "PUT update/:id" do
+    let(:attr) do
+      { id: @medium.id,
+        title: "new title",
+        description: "new description" }
+    end
+
+    before :each do
+      @medium = described_class.model.create(title: "a title", director: "an artist", rank: 20, description: "a description")
+
+      put :update, :id => @medium.id, medium_name => attr
+      @medium.reload
+    end
+
+    it "updates the #{described_class.model} record" do
+      expect(response).to redirect_to(@medium)
+      expect(@medium.title).to eq "new title"
+      expect(@medium.description).to eq "new description"
+    end
+
+    it "redirects to the #{described_class.model} show page" do
+      expect(subject).to redirect_to(polymorphic_path(@medium))
+    end
+  end
+
+
+  describe "GET #show" do
+    before :each do
+      @medium = described_class.model.create(title: "a title", director: "an artist", rank: 20, description: "a description")
+    end
+
+    it "shows the selected medium" do
+      get :show, id: @medium
+
+      expect { assigns(:medium).to eq(@medium) }
+    end
+
+    it  "renders the #show template" do
+      get :show, id: @medium
+      expect(subject).to render_template :show
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before :each do
+      @medium = described_class.model.create(title: "a title", director: "an artist", rank: 20, description: "a description")
+    end
+
+    it "deletes the record" do
+      expect{
+        delete :destroy, id: @medium
+      }.to change(described_class.model, :count).by(-1)
+    end
+
+    it "redirects to index view" do
+      delete :destroy, id: @medium
+      expect(response).to redirect_to polymorphic_path(described_class.model)
+    end
+  end
+
+  describe "PATCH #upvote" do
+    before :each do
+      @medium = described_class.model.create(title: "a title", director: "an artist", rank: 20, description: "a description")
+      patch :upvote, id: @medium
+      @medium.reload
+    end
+
+    it "increases the rank by 1" do
+      expect(@medium.rank).to eq 21
+    end
+
+    it "redirects to #{described_class.model}_path" do
+      patch :upvote, id: @medium
+      expect(response).to redirect_to polymorphic_path(@medium)
+    end
+  end
 end
