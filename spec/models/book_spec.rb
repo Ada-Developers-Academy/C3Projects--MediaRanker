2967721d6329dc11848cf963174ff4dc1590594c
amirahaile <amira.dhaile@gmail.com>
2015-07-10 14:18:52 -0700

    Made a shared examples for my models.

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,26 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  describe ":top_5 scope" do
-    before :each do
-      @excluded_album = Album.create(title: "a", ranking: 1)
-      Album.create(title: "b", ranking: 2)
-      Album.create(title: "c", ranking: 3)
-      Album.create(title: "d", ranking: 6)
-      Album.create(title: "e", ranking: 5)
-      Album.create(title: "f", ranking: 4)
-    end
-
-    it "only selects 5 items" do
-      expect(Album.top_5.count).to eq 5
-      expect(Album.top_5).to_not include(@excluded_album)
-    end
-
-    it "orders the items from high to low ranking" do
-      top_5_titles = Album.top_5.map { |album| album.title }
-      correct_order = ["d", "e", "f", "c", "b"]
-
-      expect(top_5_titles).to eq(correct_order)
-    end
-  end
+  # shared examples
+  it_behaves_like "a medium model"
 end
---
+++ spec/support/medium_model_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+RSpec.shared_examples "a medium model" do
+  describe ":top_5 scope" do
+    before :each do
+      @excluded_album = described_class.create(title: "a", ranking: 1)
+      described_class.create(title: "b", ranking: 2)
+      described_class.create(title: "c", ranking: 3)
+      described_class.create(title: "d", ranking: 6)
+      described_class.create(title: "e", ranking: 5)
+      described_class.create(title: "f", ranking: 4)
+    end
+
+    it "only selects 5 items" do
+      expect(described_class.top_5.count).to eq 5
+      expect(described_class.top_5).to_not include(@excluded_album)
+    end
+
+    it "orders the items from high to low ranking" do
+      top_5_titles = described_class.top_5.map { |described_class| described_class.title }
+      correct_order = ["d", "e", "f", "c", "b"]
+
+      expect(top_5_titles).to eq(correct_order)
+    end
+  end
+end
