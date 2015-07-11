6c0fb60405febf38af1ae6e0f6d72e85f84ea6ba
Corinne Pingul <corinnepingul@gmail.com>
2015-07-11 15:04:35 -0700

    Deleted the DELETE #destroy test from the movies controller spec.

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,22 +3,6 @@ require 'rails_helper'
 RSpec.describe MoviesController, type: :controller do
   it_behaves_like 'a MediaController', Movie, :movie, :movies_path
 
-  describe "DELETE #destroy" do
-    before :each do
-      @movie = Movie.create(title: "Harry Potter", rank: 0)
-    end
-
-    it "removes the record from the db" do
-      delete :destroy, id: @movie.id
-      expect(Movie.count).to eq 0
-    end
-
-    it "redirects to the movie index page after deleting" do
-      delete :destroy, id: @movie.id
-      expect(subject).to redirect_to(movies_path)
-    end
-  end
-
   describe "POST #create" do
     let(:movie_params) { { movie: { title: "Lord of the Rings", director: "some dude prolly" } } }
     let(:invalid_movie_params) { { movie: { director: "another dude" } } }
