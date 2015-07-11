498be2584cbb6dc890495eb16021504398efed81
Ashley Watkins <AWatkins31@gmail.com>
2015-07-10 22:32:02 -0700

    Added reminder to fill out helper specs, too!

--- spec/helpers/media_helper_spec.rb
+++ spec/helpers/media_helper_spec.rb
@@ -12,4 +12,5 @@ require 'rails_helper'
 # end
 RSpec.describe MediaHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
+  # TODO: this is pretty self-explanatory, don't you think? fill me out!
 end
