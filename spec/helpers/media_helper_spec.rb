a7a2b5dcbf0d40b969784d8b08839a1095a8115b
Ashley Watkins <AWatkins31@gmail.com>
2015-07-12 12:51:03 -0700

    Provides tests for MediaHelper's path helper methods.

--- spec/helpers/media_helper_spec.rb
+++ spec/helpers/media_helper_spec.rb
@@ -1,16 +1,63 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the MediaHelper. For example:
-#
-# describe MediaHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe MediaHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-  # TODO: this is pretty self-explanatory, don't you think? fill me out!
+  describe "path helpers" do
+    let(:movie) { Medium.create({ :title=>"Kitties Movie", :creator=>"Cat", :votes=>9, :format=>"movie" }) }
+    let(:book) { Medium.create({ :title=>"Puppies Book", :creator=>"Dog", :votes=>10, :description=>"", :format=>"book" }) }
+    let(:album) { Medium.create({ :title=>"Llamas Album", :creator=>"The Llama", :votes=>6, :description=>"", :format=>"album" }) }
+
+    describe "#object_path" do
+      it "provides correct path for a movie" do
+        expect(object_path(movie)).to eq(movie_path(movie))
+      end
+
+      it "provides correct path for a book" do
+        expect(object_path(book)).to eq(book_path(book))
+      end
+
+      it "provides correct path for an album" do
+        expect(object_path(album)).to eq(album_path(album))
+      end
+    end
+
+    describe "#new_object_path" do
+      it "provides correct path for a movie" do
+        expect(new_object_path(movie)).to eq(new_movie_path)
+      end
+
+      it "provides correct path for a book" do
+        expect(new_object_path(book)).to eq(new_book_path)
+      end
+
+      it "provides correct path for an album" do
+        expect(new_object_path(album)).to eq(new_album_path)
+      end
+    end
+    describe "#edit_object_path" do
+      it "provides correct path for a movie" do
+        expect(edit_object_path(movie)).to eq(edit_movie_path(movie))
+      end
+
+      it "provides correct path for a book" do
+        expect(edit_object_path(book)).to eq(edit_book_path(book))
+      end
+
+      it "provides correct path for an album" do
+        expect(edit_object_path(album)).to eq(edit_album_path(album))
+      end
+    end
+    describe "#objects_path" do
+      it "provides correct path for movies" do
+        expect(objects_path(movie)).to eq(movies_path)
+      end
+
+      it "provides correct path for books" do
+        expect(objects_path(book)).to eq(books_path)
+      end
+
+      it "provides correct path for albums" do
+        expect(objects_path(album)).to eq(albums_path)
+      end
+    end
+  end
 end
