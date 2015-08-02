51726edbd1815b9967dfc2b301ec5368b9f44f36
Carly Jugler <carjug@gmail.com>
2015-08-02 13:41:01 -0700

    Adds order_desc scope to movie model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
 
-    @movies = @movies.order('rank DESC')
+    @movies = @movies.order_desc
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,6 +8,8 @@ class Movie < ActiveRecord::Base
             numericality: { only_integer: true, less_than: 200 }
 
 # Scopes!
+   scope :order_desc, -> { order('rank DESC') }
+
   scope :best, -> { order('rank DESC').limit(10) }
 
 end
