a0ef70967767d7305801e6267a0c4ed05cc2a265
Carly Jugler <carjug@gmail.com>
2015-08-02 13:39:56 -0700

    Creates scope for order_desc in book model.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
 
-    @books = @books.order('rank DESC')
+    @books = @books.order_desc
   end
 
   def new
--- app/models/book.rb
+++ app/models/book.rb
@@ -8,5 +8,7 @@ class Book < ActiveRecord::Base
             numericality: { only_integer: true, less_than: 200 }
 
 # Scopes!
+  scope :order_desc, -> { order('rank DESC') }
+
   scope :best, -> { order('rank DESC').limit(10) }
 end
