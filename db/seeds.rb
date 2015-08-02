5b28b287a5c129149fd5d1bf0e3bc1c784911647
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-02 15:30:31 -0700

    Seeds whitespace.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,16 @@
 
 # MOVIES -----------------------------------------------------------------------
 movies = [
-          { name: "Gattaca", creator: "Andrew Niccol",
-          description: "A genetically inferior man assumes the identity of a superior one in order to pursue his lifelong dream of space travel."
-          } ,
-          { name: "The Fifth Element", creator: "Luc Besson",
-          description: "In the colorful future, a cab driver unwittingly becomes the central figure in the search for a legendary cosmic weapon to keep Evil and Mr Zorg at bay."
-          } ,
+  {
+    name: "Gattaca",
+    creator: "Andrew Niccol",
+    description: "A genetically inferior man assumes the identity of a superior one in order to pursue his lifelong dream of space travel."
+  },
+  {
+    name: "The Fifth Element",
+    creator: "Luc Besson",
+    description: "In the colorful future, a cab driver unwittingly becomes the central figure in the search for a legendary cosmic weapon to keep Evil and Mr Zorg at bay."
+  }
 ]
 
 movies.each { |movie| Movie.create(movie) }
@@ -21,8 +25,16 @@ movies.each { |movie| Movie.create(movie) }
 # ALBUMS ----------------------------------------------------------------------
 
 albums = [
-  {name: "The Soul Cages", creator: "Sting", description: "About a mariner"},
-  {name: "Chicken-n-Beer", creator: "Ludacris", description: "Rap"}
+  {
+    name: "The Soul Cages",
+    creator: "Sting",
+    description: "About a mariner"
+  },
+  {
+    name: "Chicken-n-Beer",
+    creator: "Ludacris",
+    description: "Rap"
+  }
 ]
 
 albums.each { |album| Album.create(album) }
@@ -30,10 +42,16 @@ albums.each { |album| Album.create(album) }
 # BOOKS -----------------------------------------------------------------------
 
 books = [
-  {name: "Harry Potter and the Deathly Hallows", creator: "J. K. Rowling",
-  description: "The saga concludes." },
-  { name: "Going Postal", creator: "Terry Pratchett",
-    description: "The Postal Service has never been more exciting."}
+  {
+    name: "Harry Potter and the Deathly Hallows",
+    creator: "J. K. Rowling",
+    description: "The saga concludes."
+  },
+  {
+    name: "Going Postal",
+    creator: "Terry Pratchett",
+    description: "The Postal Service has never been more exciting."
+  }
 ]
 
 books.each { |book| Book.create(book) }
