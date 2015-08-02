45c589da236b5f9d621e53be54fdc7e7b73f1b13
Lila Garcia <lilagrc@gmail.com>
2015-08-02 14:35:40 -0700

    Removed unncessary let definition

--- spec/support/media_shared_controller_spec.rb
+++ spec/support/media_shared_controller_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 RSpec.shared_examples "a media" do
 
-  let(:medium) { described_class.model.new() }
-
   describe "GET index" do
     it "has a 200 status code" do
       get :index
