deb37449654bc5086af565e195476d0cd6261342
Elsa <elsa.moluf@gmail.com>
2015-08-02 15:23:55 -0700

    Set top media limit to constant.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
+
+  TOP_LIMIT = 3
+
   def index
-    @top_albums = Album.top
-    @top_books = Book.top
-    @top_movies = Movie.top
+    @top_albums = Album.top(TOP_LIMIT)
+    @top_books = Book.top(TOP_LIMIT)
+    @top_movies = Movie.top(TOP_LIMIT)
   end
 end
