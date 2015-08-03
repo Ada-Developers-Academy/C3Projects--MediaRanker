3c71a55a248472c33ff56e1168756a5db2d63f1b
Elia <elgrenier@gmail.com>
2015-08-02 22:06:14 -0700

    tried to refactor to inherit from parent methods in application controller. may have broken everything

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,53 +1,16 @@
 class AlbumsController < ApplicationController
 
-  def index
-    @media = Album.all.order(rank: :desc)
-    # defining medium allows me to use polymorphic paths for new from the index page
-    @medium = Album.new
-  end
-
-  def show
-    @media = Album.find(params[:id])
-  end
-
-    def new
-    @media = Album.new
-    end
-
   def create
-    @media = Album.new(create_params[:album])
-    @media.save
-    redirect_to album_path(@media)
-  end
-
-  def edit
-    @media = Album.find(params[:id])
+    @medium.create(create_params[:album])
   end
 
   def update
-    @media = Album.find(params[:id])
     @media.update(create_params[:album])
-    redirect_to album_path(@media)
-  end
-
-  def destroy
-    @media = Album.find(params[:id])
-    @media.destroy
-    redirect_to albums_path
-  end
-
-  def upvote
-    @media = Album.find(params[:id])
-    @media.add_a_vote
-    @media.save
-    render :show
   end
 
   private
 
-  def create_params
-    params.permit(album: [:name, :creator, :description, :rank])
-  end
-
-
+    def create_params
+      params.permit(album: [:name, :creator, :description, :rank])
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,51 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action: :find_all_media, only: [:index]
+  before_action: :create_medium, only: [:index, :new, :create]
+  before_action: :find_media, only: [:show, :edit, :update, :destroy, :upvote]
+
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def create
+    @medium.create(create_params[:media])
+    redirect_to polymorphic_path(@medium)
+  end
+
+  def edit; end
+
+
+  def update
+    @media.update(create_params[:media])
+    redirect_to polymorphic_path(@media)
+  end
+
+  def destroy
+    @media.destroy
+    redirect_to polymorphic_path(@media.class)
+  end
+
+  def upvote
+    @media.add_a_vote
+    @media.save
+    render :show
+  end
+
+  def find_all_media
+    @all_media = self.all.order(rank :desc)
+  end
+
+  def find_media
+    # find a specific instance based on params
+    @media = self.find(params[:id])
+  end
+
+  def create_medium
+    # medium is the new instance
+    @medium = self.new
+  end
 end
