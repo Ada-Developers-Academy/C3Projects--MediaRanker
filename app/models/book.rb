3672f77534e9c184eda17e70f1e4984665ce67ff
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-03 11:45:15 -0700

    Edited scopes.

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,8 +5,8 @@ class Book < ActiveRecord::Base
   # validates :description
   validates :rank, presence: true, numericality: { only_integer: true }
 
-  scope :best_first, -> (total) { order('rank DESC').limit(total) }
   scope :best_first_all, -> { order('rank DESC') }
+  scope :best_first, -> (total) { best_first_all.limit(total) }
 
   before_validation :set_rank
 
