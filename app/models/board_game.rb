d187de710ac96a63d3ad6db2e275747dbb1f32ff
Brenna <brennaleker@gmail.com>
2015-08-02 19:25:21 -0700

    refactoring yay

--- app/models/board_game.rb
+++ app/models/board_game.rb
@@ -2,9 +2,4 @@ class BoardGame < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
 
   scope :ordered_by_rating, -> { order('board_games.rating DESC') }
-
-  def locate_board_game
-    board_game_id = params[:id]
-    @board_game = BoardGame.find(board_game_id)
-  end
 end
