ae208d0fb88dc4d7331d75671b831b428e541a4b
Corinne Pingul <corinnepingul@gmail.com>
2015-08-02 19:37:34 -0600

    changes rank_order_top scope

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @movies = Movie.all.rank_order_top_ten
-    @books = Book.all.rank_order_top_ten
-    @albums = Album.all.rank_order_top_ten
+    @movies = Movie.all.rank_order_top
+    @books = Book.all.rank_order_top
+    @albums = Album.all.rank_order_top
 
     @media = [ @movies, @books, @albums ]
 
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,10 @@
 class Album < ActiveRecord::Base
+  TOP_NUM = 10
+
   # Validations --------------------------------
   validates :title, presence: true
 
   # Scope --------------------------------------
   scope :rank_order, -> { order(rank: :desc)}
-  scope :rank_order_top_ten, -> { rank_order.limit(10)}
+  scope :rank_order_top, -> { rank_order.limit(TOP_NUM)}
 end
