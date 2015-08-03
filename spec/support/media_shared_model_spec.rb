6c7b6183ebc9fb31e4fb8faf339eb2c7fce868b0
Shanna Chau <shannachau@gmail.com>
2015-08-02 18:53:03 -0700

    Added test for ranking_order scope

--- spec/support/media_shared_model_spec.rb
+++ spec/support/media_shared_model_spec.rb
@@ -20,13 +20,28 @@ RSpec.shared_examples "a media model" do
       @media4 = described_class.create(name: 'whatevah', ranking: 4)
     end
 
-    it "orders movies according to ranking, highest to lowest" do
+    it "orders media according to ranking, highest to lowest" do
       top_media = [@media4, @media1, @media3]
       expect(described_class.top(3)).to eq(top_media)
     end
 
     it "only displays given number of movies" do
       expect(described_class.top(3).count).to eq 3
+    end
+  end
+
+  describe "ranking_order scope" do
+    before :each do
+      @media1 = described_class.create(name: 'whatever', ranking: 1)
+      @media2 = described_class.create(name: 'whatever', ranking: 2)
+      @media3 = described_class.create(name: 'whatevz',  ranking: 3)
+      @media4 = described_class.create(name: 'whatevah', ranking: 4)
+    end
+
+    it "orders all media in model according to ranking, highest to lowest" do
+      ranked_media = [@media4, @media3, @media2, @media1]
+
+      expect(described_class.ranking_order). to eq(ranked_media)
     end
   end
 
