3dc5e6fd5d0c071361dbfe04105ea2cc71cd19e1
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-02 19:36:22 -0700

    Removed 'all' from scope.

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,7 +5,7 @@ class Album < ActiveRecord::Base
   # validates :description
   validates :rank, presence: true, numericality: { only_integer: true }
 
-  scope :best_first, -> (total) { all.order('rank DESC').limit(total) }
-  scope :best_first_all, -> { all.order('rank DESC') }
+  scope :best_first, -> (total) { order('rank DESC').limit(total) }
+  scope :best_first_all, -> { order('rank DESC') }
 
 end
